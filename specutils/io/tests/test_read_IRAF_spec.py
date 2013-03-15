62f7bd7cb1987d663186dae097330f909d666a1b
hamogu <moritz.guenther@gmx.de>
2013-03-15 12:25:29 -0400

    Implement reader for REDUCE files, superceeds #14

---
+++ specutils/io/tests/test_read_IRAF_spec.py
@@ -0,0 +1,17 @@
+import astropy.io.fits as fits
+import astropy.io.ascii as ascii
+from . import read_IRAF_spec
+
+from .common import  assert_almost_equal
+
+
+
+def test_multispec_legendre():
+    iraf = ascii.read('t/TRES.dat', data_start = 127, Reader = ascii.NoHeader, names = ['wave', 'flux'])
+    spec = read_IREAF_spec.read_IRAF_spec('t/TRES.fits')
+    assert_almost_equal(iraf['wave'], spec.dispersion[10,:])
+
+def test_multispec_chebyshev():
+    iraf = ascii.read('t/AAO_11.txt', data_start = 175, Reader = ascii.NoHeader, names = ['wave', 'flux'])
+    spec = read_IREAF_spec.read_IRAF_spec('t/AAO.fits')
+    assert_almost_equal(iraf['wave'], spec.dispersion[10,:])
