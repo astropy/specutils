211f02b8dcc87d4abbb8a3c83d0638fb2cad7018
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2014-02-17 16:32:13 +0100

    added a test

---
+++ specutils/utils/setup_package.py
@@ -0,0 +1,5 @@
+def get_package_data():
+    # Installs the testing data files. Unable to get package_data
+    # to deal with a directory hierarchy of files, so just explicitly list.
+    return {'specutils.utils.tests': ['data/idexample.dat']
+            }
---
+++ specutils/utils/tests/test_iraf_line_reader.py
@@ -0,0 +1,8 @@
+from .. import IRAF_id_reader
+
+def data_path(filename):
+        data_dir = os.path.join(os.path.dirname(__file__), 'data')
+        return os.path.join(data_dir, filename)
+
+def test_iraf_line_reader():
+    IRAF_id_reader.IRAF_identify_reader(data_path('idexample'))
