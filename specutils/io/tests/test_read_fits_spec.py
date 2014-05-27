5006a4bc580ab63516e58f192ade4256116ae157
Shailesh Ahuja <shailesh.ahuja03@gmail.com>
2014-05-27 20:18:46 +0800

    Changing the assertion to np.all_close

--- specutils/io/tests/test_read_fits_spec.py
+++ specutils/io/tests/test_read_fits_spec.py
@@ -7,7 +7,6 @@ import numpy as np
 
 from specutils.io import read_fits
 
-from astropy.io.ascii.tests.common import assert_almost_equal
 
 
 def data_path(filename):
@@ -18,24 +17,24 @@ def test_multispec_legendre():
     iraf = ascii.read(data_path('TRES.dat'), data_start = 127, Reader = ascii.NoHeader, names = ['wave', 'flux'])
     spectra = read_fits.read_fits_multispec_to_list(data_path('TRES.fits'))
     spec = spectra[10]
-    assert_almost_equal(iraf['wave'], spec.dispersion.value)
+    np.testing.assert_allclose(iraf['wave'], spec.dispersion.value)
     assert spec.dispersion.unit == u.Angstrom
 
 def test_multispec_chebyshev():
     iraf = ascii.read(data_path('AAO_11.txt'), data_start = 175, Reader = ascii.NoHeader, names = ['wave', 'flux'])
     spectra = read_fits.read_fits_multispec_to_list(data_path('AAO.fits'))
     spec = spectra[10]
-    assert_almost_equal(iraf['wave'], spec.wavelength.value)
+    np.testing.assert_allclose(iraf['wave'], spec.wavelength.value)
 
 def test_1dspec_UVES():
     spec = read_fits.read_fits_spectrum1d(data_path('UVES.fits'))
     iraf = ascii.read(data_path('uves_iraf_read_truncated.dat'), names=['index', 'wave', 'flux'])
-    assert_almost_equal(spec.dispersion[iraf['index']], iraf['wave'])
+    np.testing.assert_allclose(spec.dispersion[iraf['index']], iraf['wave'])
 
     assert not hasattr(spec.dispersion, 'unit')
 
 def test_1dspec_vrad():
     iraf = ascii.read(data_path('gbt_1d_iraf_read.dat'), names=['wave', 'flux'])
     spec = read_fits.read_fits_spectrum1d(data_path('gbt_1d.fits'))
-    assert_almost_equal(iraf['wave'], spec.dispersion.value)
+    np.testing.assert_allclose(iraf['wave'], spec.dispersion.value)
     assert spec.dispersion.unit == u.Unit('km/s')
