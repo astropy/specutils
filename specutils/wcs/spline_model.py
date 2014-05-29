296b2582c813fd17ce1beb82774b3fec17540917
Shailesh Ahuja <shailesh.ahuja03@gmail.com>
2014-05-29 18:01:33 +0800

    Using scipy's splev function

--- specutils/wcs/spline_model.py
+++ specutils/wcs/spline_model.py
@@ -1,1 +1,14 @@
 __author__ = 'Shailesh'
+
+from astropy.modeling import Model, polynomial
+from scipy import interpolate
+
+class SplineModel(polynomial.PolynomialModel):
+    def __init__(self, degree, knots, coefficients):
+        self.knots = knots
+        self.coefficients = coefficients
+        super(SplineModel, self).__init__(self, degree, n_inputs=1, n_outputs=1, param_dim=1)
+
+    def __call__(self, x):
+        return interpolate.splev(x, (self.knots, self.coefficients, self.degree))
+
