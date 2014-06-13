51d2493e15adb6afe93f37df7161ec6c9f3d82ea
Shailesh Ahuja <shailesh.ahuja03@gmail.com>
2014-06-13 16:58:21 +0530

    Adding another test case (currently failing)

--- specutils/wcs/specwcs.py
+++ specutils/wcs/specwcs.py
@@ -225,9 +225,9 @@ class Spectrum1DPolynomialWCS(BaseSpectrum1DWCS, polynomial.Polynomial1D):
                                                       window=window, **params)
         self.unit = unit
 
-        self.fits_header_writers = {'linear': self.write_fits_header_linear,
-                                    'matrix': self.write_fits_header_matrix,
-                                    'multispec': self.write_fits_header_multispec}
+        self.fits_header_writers = {'linear': self._write_fits_header_linear,
+                                    'matrix': self._write_fits_header_matrix,
+                                    'multispec': self._write_fits_header_multispec}
 
     def __call__(self, pixel_indices):
         return super(Spectrum1DPolynomialWCS, self).__call__(
@@ -236,7 +236,7 @@ class Spectrum1DPolynomialWCS(BaseSpectrum1DWCS, polynomial.Polynomial1D):
     def write_fits_header(self, header, spectral_axis=1, method='linear'):
         self.fits_header_writers[method](header, spectral_axis)
 
-    def write_fits_header_linear(self, header, spectral_axis=1):
+    def _write_fits_header_linear(self, header, spectral_axis=1):
         header['cdelt{0}'.format(spectral_axis)] = self.c1.value
         header['crval{0}'.format(spectral_axis)] = self.c0.value
 
@@ -250,7 +250,7 @@ class Spectrum1DPolynomialWCS(BaseSpectrum1DWCS, polynomial.Polynomial1D):
 
             header['cunit{0}'.format(spectral_axis)] = unit_string
 
-    def write_fits_header_matrix(self, header, spectral_axis=1):
+    def _write_fits_header_matrix(self, header, spectral_axis=1):
         header['cd{0}_{1}'.format(spectral_axis, spectral_axis)] = self.c1.value
         header['crval{0}'.format(spectral_axis)] = self.c0.value
 
@@ -265,8 +265,9 @@ class Spectrum1DPolynomialWCS(BaseSpectrum1DWCS, polynomial.Polynomial1D):
             header['cunit{0}'.format(spectral_axis)] = unit_string
 
     # can only be implemented, when the reader is in place
-    def write_fits_header_multispec(self, header, spectral_axis=1):
+    def _write_fits_header_multispec(self, header, spectral_axis=1):
         pass
+
 
 class Spectrum1DIRAFLegendreWCS(BaseSpectrum1DWCS, polynomial.Legendre1D):
     """
@@ -340,12 +341,11 @@ class Spectrum1DIRAFCombinationWCS(BaseSpectrum1DWCS):
     Dispersion = Sum over all WCS
         [Weight * (Zero point offset + WCS(pixels)) / (1 + doppler factor)]
     """
-    def __init__(self, num_pixels, aperture=1, beam=88, aperture_low=0.0,
-                 aperture_high=0.0, doppler_factor=0.0, unit=None):
+    def __init__(self, aperture=1, beam=88, aperture_low=0.0, aperture_high=0.0,
+                 doppler_factor=0.0, unit=None):
         self.wcs_list = []
         self.aperture = aperture
         self.beam = beam
-        self.num_pixels = num_pixels
         self.aperture_low = aperture_low
         self.aperture_high = aperture_high
         self.doppler_factor = doppler_factor
