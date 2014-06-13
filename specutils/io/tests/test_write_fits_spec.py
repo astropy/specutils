d86df8b0956b0bd7b015bca662684e0188e78d88
Shailesh Ahuja <shailesh.ahuja03@gmail.com>
2014-06-13 16:58:21 +0530

    Adding another test case (currently failing)

--- specutils/io/tests/test_write_fits_spec.py
+++ specutils/io/tests/test_write_fits_spec.py
@@ -6,8 +6,18 @@ def data_path(filename):
     data_dir = os.path.join(os.path.dirname(__file__), 'files')
     return os.path.join(data_dir, filename)
 
-def test_linear_write():
+def test_linear_write1():
     spec = read_fits.read_fits_spectrum1d(data_path('UVES.fits'))
+    data = spec.data
+    wcs = spec.wcs
+
+    write_fits.write(data_path('test.fits'), data, wcs)
+    test_spec = read_fits.read_fits_spectrum1d(data_path('test.fits'))
+    np.testing.assert_allclose(spec.data, test_spec.data)
+    np.testing.assert_allclose(spec.dispersion, test_spec.dispersion)
+
+def test_linear_write2():
+    spec = read_fits.read_fits_spectrum1d(data_path('gbt_1d.fits'))
     data = spec.data
     wcs = spec.wcs
 
