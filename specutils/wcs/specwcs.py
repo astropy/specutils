b2215b6a5e30028c30e7e68d7f912eea32fbe209
Shailesh Ahuja <shailesh.ahuja03@gmail.com>
2014-06-13 16:34:20 +0530

    Basic linear writer and tests; fixed reading bug in fits reader

--- specutils/wcs/specwcs.py
+++ specutils/wcs/specwcs.py
@@ -12,12 +12,12 @@ import astropy.units as u
 
 from astropy.utils.misc import deprecated
 from astropy.utils import OrderedDict
+from astropy.io import fits
 
 
 ##### Delete at earliest convenience (currently deprecated)
 #### VVVVVVVVVV
 valid_spectral_units = [u.pix, u.km / u.s, u.m, u.Hz, u.erg]
-
 
 @deprecated('0.dev???', 'using no units is now allowed for WCS')
 def check_valid_unit(unit):
@@ -228,6 +228,19 @@ class Spectrum1DPolynomialWCS(BaseSpectrum1DWCS, polynomial.Polynomial1D):
     def __call__(self, pixel_indices):
         return super(Spectrum1DPolynomialWCS, self).__call__(
             pixel_indices) * self.unit
+
+    def add_to_header(self, header, spectral_axis=1):
+        header['cdelt{0}'.format(spectral_axis)] = self.c1.value
+        header['cd{0}_{1}'.format(spectral_axis, spectral_axis)] = self.c1.value
+        header['crval{0}'.format(spectral_axis)] = self.c0.value
+        unit_string = self.unit
+        if isinstance(self.unit, u.Unit):
+            if self.unit == u.AA:
+                unit_string = 'angstroms'
+            else:
+                unit_string = self.unit.name
+
+        header['crunit{0}'.format(spectral_axis)] = unit_string
 
 
 class Spectrum1DIRAFLegendreWCS(BaseSpectrum1DWCS, polynomial.Legendre1D):
