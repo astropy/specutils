9b2233de6dba8f9320dba6f424d2d87f725f4837
Wolfgang Kerzendorf <wkerzendorf@gmail.com>
2014-06-26 16:38:46 -0400

    added test files to setup_package

--- specutils/io/setup_package.py
+++ specutils/io/setup_package.py
@@ -2,6 +2,9 @@ def get_package_data():
     # Installs the testing data files. Unable to get package_data
     # to deal with a directory hierarchy of files, so just explicitly list.
     return {'specutils.io.tests': ['files/lwp11854.mxlo', 'files/swp02283.mxlo',
-                                   'files/1d.fits', 'files/AAO.fits', 'files/AAO_11.txt', 'files/TRES.dat',
-                                   'files/TRES.fits', 'files/UVES.fits', 'files/lwp11854.mxlo',
-                                   'files/swp02283.mxlo']}
+                                   'files/1d.fits', 'files/AAO.fits',
+                                   'files/AAO_11.txt', 'files/TRES.dat',
+                                   'files/TRES.fits', 'files/UVES.fits',
+                                   'files/lwp11854.mxlo', 'files/swp02283.mxlo',
+                                   'files/gbt_1d.fits',
+                                   'files/gbt_1d_iraf_read.dat']}
