Shifting computation to from_doppler_factor class method, fixing tests

specutils/models/tests/test_models.py
@@ -10,23 +10,24 @@ minversion = pytest.mark.skipif(version[0] == '0' and version[1] < '4',
                                 reason="at least astropy-0.4 required")
 
 def test_doppler():
-    doppler_factor = 1.38
-    wcs = specwcs.DopplerShift(doppler_factor)
-    x = np.array([1.25, 4.3])
-    y = x* doppler_factor
-    np.testing.assert_allclose(y, wcs(x))
-
-def test_doppler_velocity():
     velocity = 1.38 * u.meter / u.second
-    wcs = specwcs.DopplerShift.from_velocity(velocity)
-    x = np.array([1.25, 4.3])
     beta = velocity / constants.c
     doppler_factor = ((1 + beta)/(1-beta)) ** 0.5
+    wcs = specwcs.DopplerShift(velocity)
+    x = np.array([1.25, 4.3])
+    y = x * doppler_factor
+    np.testing.assert_allclose(y, wcs(x))
+
+def test_doppler_factor():
+    doppler_factor = 1.38
+    wcs = specwcs.DopplerShift.from_doppler_factor(doppler_factor)
+    x = np.array([1.25, 4.3])
     y = x * doppler_factor
     np.testing.assert_allclose(y, wcs(x))
 
 def test_composite_wcs():
-    doppler_wcs = specwcs.DopplerShift(1.38)
+    velocity = 1.38 * u.meter / u.second
+    doppler_wcs = specwcs.DopplerShift(velocity)
     log_wcs = lambda x: 10 ** x
 
     def add_one(x):
@@ -37,8 +38,10 @@ def test_composite_wcs():
     np.testing.assert_allclose(y, wcs(x))
 
 def test_weighted_wcs():
-    doppler_wcs1 = specwcs.DopplerShift(1.38)
-    doppler_wcs2 = specwcs.DopplerShift(0.45)
+    v1 = 1.38 * u.meter / u.second
+    v2 = 0.45 * u.meter / u.second
+    doppler_wcs1 = specwcs.DopplerShift(v1)
+    doppler_wcs2 = specwcs.DopplerShift(v2)
     weight1 = 1.2
     weight2 = 2
     zero_offset1 = 1