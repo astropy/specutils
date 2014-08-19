bba8b98567daa9e00662696dfe0cc560baea692c
Shailesh Ahuja <shailesh.ahuja03@gmail.com>
2014-08-19 12:41:19 +0800

    Removing deprecated code

--- specutils/wcs/specwcs.py
+++ specutils/wcs/specwcs.py
@@ -10,7 +10,6 @@ from astropy.modeling.parameters import Parameter
 from ..models.BSplineModel import BSplineModel
 import astropy.units as u
 
-from astropy.utils.misc import deprecated
 from astropy.utils import OrderedDict
 from astropy.io import fits
 import copy
@@ -20,14 +19,6 @@ import math
 ##### Delete at earliest convenience (currently deprecated)
 #### VVVVVVVVVV
 valid_spectral_units = [u.pix, u.km / u.s, u.m, u.Hz, u.erg]
-
-@deprecated('0.dev???', 'using no units is now allowed for WCS')
-def check_valid_unit(unit):
-    if not any([unit.is_equivalent(x) for x in valid_spectral_units]):
-        raise ValueError(
-            "Unit %r is not recognized as a valid spectral unit.  Valid units are: " % unit.to_string() +
-            ", ".join([x.to_string() for x in valid_spectral_units]))
-
 
 #^^^^^^^^^^^^^^^^^
 class Spectrum1DWCSError(Exception):
@@ -163,58 +154,6 @@ class Spectrum1DLookupWCS(BaseSpectrum1DWCS):
         else:
             raise NotImplementedError(
                 'Interpolation type %s is not implemented' % self.lookup_table_interpolation_kind)
-
-
-class Spectrum1DLinearWCS(BaseSpectrum1DWCS):
-    """
-    A simple linear wcs
-    """
-
-    dispersion0 = Parameter('dispersion0')
-    dispersion_delta = Parameter('dispersion_delta')
-
-    @deprecated('0.dev??', message='please use Spectrum1DPolynomialWCS')
-    def __init__(self, dispersion0, dispersion_delta, pixel_index, unit):
-        super(Spectrum1DLinearWCS, self).__init__()
-
-        #### Not clear what to do about units of dispersion0 and dispersion_delta.
-        # dispersion0 should have units like angstrom, whereas dispersion_delta should have units like angstrom/pix
-        # for now I assume pixels don't have units and both dispersion0 and dispersion_delta should have the same unit
-        dispersion0 = u.Quantity(dispersion0, unit)
-        dispersion_delta = u.Quantity(dispersion_delta, unit)
-
-        check_valid_unit(dispersion0.unit)
-        check_valid_unit(dispersion_delta.unit)
-
-
-        ##### Quick fix - needs to be fixed in modelling ###
-        if unit is None:
-            unit = dispersion0.unit
-
-        self.unit = unit
-
-        self.dispersion0 = dispersion0.value
-        self.dispersion_delta = dispersion_delta.value
-        self.pixel_index = pixel_index
-
-
-    def __call__(self, pixel_indices):
-        if misc.isiterable(pixel_indices) and not isinstance(pixel_indices,
-                                                             six.string_types):
-            pixel_indices = np.array(pixel_indices)
-        return (self.dispersion0 + self.dispersion_delta * (
-            pixel_indices - self.pixel_index)) * self.unit
-
-    def invert(self, dispersion_values):
-        if not hasattr(dispersion_values, 'unit'):
-            raise u.UnitsException(
-                'Must give a dispersion value with a valid unit (i.e. quantity 5 * u.Angstrom)')
-
-        if misc.isiterable(dispersion_values) and not isinstance(
-                dispersion_values, six.string_types):
-            dispersion_values = np.array(dispersion_values)
-        return float((
-                         dispersion_values - self.dispersion0) / self.dispersion_delta) + self.pixel_index
 
 
 class Spectrum1DPolynomialWCS(BaseSpectrum1DWCS, polynomial.Polynomial1D):
@@ -576,16 +515,3 @@ class DopplerShift(Model):
     def doppler_factor(self):
         return math.sqrt((1 + self.beta)/(1 - self.beta))
 
-
-@deprecated('0.dev???')
-def _parse_doppler_convention(dc):
-    dcd = {'relativistic': u.doppler_relativistic,
-           'radio': u.doppler_radio,
-           'optical': u.doppler_optical}
-    if dc in dcd:
-        return dcd[dc]
-    elif dc in dcd.values():  # allow users to specify the convention directly
-        return dc
-    else:
-        raise ValueError(
-            "Doppler convention must be one of " + ",".join(dcd.keys()))
