e5167c1342e675a0e161b3af58b3921bd14326cd
ibusko <busko@stsci.edu>
2016-02-15 10:21:24 -0500

    Added ability to read a FITS file from the command line. This is a bare-bones implementation just to make my development workflow a bit easier. Perhaps we shouldn't even advertise this as a feature for this release.

--- pyfocal/app.py
+++ pyfocal/app.py
@@ -15,17 +15,19 @@ from .ui.controller import Controller
 
 
 class App(object):
-    def __init__(self):
+    def __init__(self, argv):
         super(App, self).__init__()
         self.viewer = Viewer()
         self.controller = Controller(self.viewer)
 
+        if len(argv) > 1:
+            self.controller.read_FITS(sys.argv[1])
 
 def main():
     qapp = QApplication(sys.argv)
     # qapp.setGraphicsSystem('native')
 
-    app = App()
+    app = App(sys.argv)
     app.viewer.show()
     sys.exit(qapp.exec_())
 
