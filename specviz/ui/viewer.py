d8b9fb4ad01e65b98a69325b1236bd280fb1bc67
busko <busko@stsci.edu>
2016-02-22 12:52:18 -0500

    Disable both Load and Save buttons when no layer is selected.

--- specviz/ui/viewer.py
+++ specviz/ui/viewer.py
@@ -60,6 +60,8 @@ class Viewer(QMainWindow):
             self.main_window.createModelLayerButton.hide()
             self.main_window.updateModelLayerButton.hide()
             self.main_window.fittingRoutinesGroupBox.setEnabled(False)
+            self.main_window.loadModelButton.setEnabled(False)
+            self.main_window.saveModelButton.setEnabled(False)
 
         if not hasattr(layer, 'model'):
             self.main_window.createModelLayerButton.show()
