5a590235c125bd9fd5699afb3b8e9ebb2ca3a41d
Pey Lian Lim <lim@stsci.edu>
2016-02-29 15:46:36 -0500

    Catch KeyboardInterrupt and exit gracefully

--- specviz/app.py
+++ specviz/app.py
@@ -1,15 +1,21 @@
-"""
-SpecViz front-end GUI access point.
-
+"""SpecViz front-end GUI access point.
 This script will start the GUI.
+
 """
 from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
 
+# STDLIB
+import signal
 import sys
+import warnings
 
+# THIRD-PARTY
+from astropy.utils.exceptions import AstropyUserWarning
+
+# LOCAL
 from .third_party.qtpy.QtWidgets import *
-
+from .third_party.qtpy.QtCore import QTimer
 from .ui.viewer import Viewer
 from .ui.controller import Controller
 
@@ -26,6 +32,11 @@ class App(object):
 def setup():
     qapp = QApplication(sys.argv)
     # qapp.setGraphicsSystem('native')
+
+    #http://stackoverflow.com/questions/4938723/what-is-the-correct-way-to-make-my-pyqt-application-quit-when-killed-from-the-co
+    timer = QTimer()
+    timer.start(500)  # You may change this if you wish.
+    timer.timeout.connect(lambda: None)  # Let the interpreter run each 500 ms.
 
     app = App(sys.argv)
     app.viewer.show()
@@ -48,8 +59,16 @@ def main():
     """
     Used when launching the application as standalone.
     """
+    signal.signal(signal.SIGINT, sigint_handler)
     qapp, app = setup()
     sys.exit(qapp.exec_())
+
+
+def sigint_handler(*args):
+    """Handler for the SIGINT signal."""
+    warnings.warn('KeyboardInterrupt caught; specviz will terminate',
+                  AstropyUserWarning)
+    QApplication.quit()
 
 
 if __name__ == '__main__':
