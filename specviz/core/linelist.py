d22924bbfba8571388b4541dc026db83f4c7a566
ibusko <busko@stsci.edu>
2016-05-02 16:11:24 -0400

    Line lists must work with units.

--- specviz/core/linelist.py
+++ specviz/core/linelist.py
@@ -1,5 +1,16 @@
+from __future__ import (absolute_import, division, print_function,
+                        unicode_literals)
+
+import numpy as np
+
 from astropy.table import Table
 
+COLUMN_NAME = 'name'
+COLUMN_START = 'start'
+COLUMN_END = 'end'
+WAVELENGTH_COLUMN = 'wavelength'
+ID_COLUMN = 'ids'
+UNITS_COLUMN = 'units'
 
 class LineList(Table):
 
@@ -8,3 +19,9 @@ class LineList(Table):
         from ..interfaces.registries import io_registry
         return io_registry.read(cls, *args, **kwargs)
 
+    def extract_range(self, wmin, wmax):
+        wavelengths = self[WAVELENGTH_COLUMN]
+
+        indices = np.where(wavelengths.all() >= wmin and wavelengths.all() <= wmax)
+
+        print("@@@@@@  file linelist.py; line 26 - ",  wmin, wmax, wavelengths.unit, indices)
