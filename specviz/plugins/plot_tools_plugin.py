08a8811573688ef9f80030defbf8f78e65c4633f
busko <busko@stsci.edu>
2016-06-14 13:24:02 -0400

    Iterim commit. Work in progress.....

--- specviz/plugins/plot_tools_plugin.py
+++ specviz/plugins/plot_tools_plugin.py
@@ -6,7 +6,7 @@ from astropy.units import Unit
 from ..core.comms import Dispatch, DispatchHandle
 from ..ui.widgets.utils import ICON_PATH
 from ..ui.widgets.plugin import Plugin
-from ..ui.widgets.dialogs import TopAxisDialog, UnitChangeDialog
+from ..ui.widgets.dialogs import TopAxisDialog, UnitChangeDialog, LineListsWindow
 
 
 class PlotToolsPlugin(Plugin):
@@ -17,6 +17,7 @@ class PlotToolsPlugin(Plugin):
     def setup_ui(self):
         self._top_axis_dialog = TopAxisDialog()
         self._unit_change_dialog = UnitChangeDialog()
+        self._line_lists_window = LineListsWindow()
 
         # Add an roi
         self.button_add_roi = self.add_tool_bar_actions(
@@ -50,6 +51,7 @@ class PlotToolsPlugin(Plugin):
             description='Add line labels',
             icon_path=os.path.join(ICON_PATH, "Label-48.png"),
             category='Selections',
+            # callback=self._show_linelists_window,
             callback=Dispatch.on_requested_linelist.emit,
             enabled=False)
 
@@ -92,6 +94,18 @@ class PlotToolsPlugin(Plugin):
                 ref_wave=self._top_axis_dialog.ref_wave)
         else:
             logging.warning("Active window does not have any plots.")
+
+
+#TODO work in progress
+    def _show_linelists_window(self):
+        # self._line_lists_dialog.show()
+
+        print ('@@@@@@     line: 100  - ')
+
+        Dispatch.on_requested_linelist.emit,
+
+
+
 
     @DispatchHandle.register_listener("on_activated_window")
     def toggle_enabled(self, window):
