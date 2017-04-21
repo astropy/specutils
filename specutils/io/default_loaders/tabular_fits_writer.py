14e5bf47beaa471e7698669aa1d33a29b1486f73
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2017-04-21 11:00:48 -0400

    rename the writer too

--- specutils/io/default_loaders/tabular_fits_writer.py
+++ specutils/io/default_loaders/tabular_fits_writer.py
@@ -2,8 +2,8 @@ from astropy.table import Table
 from specutils.io.registers import custom_writer
 
 
-@custom_writer("generic-fits-writer")
-def generic_fits(spectrum, file_name, **kwargs):
+@custom_writer("tabular-fits-writer")
+def tabular_fits(spectrum, file_name, **kwargs):
     flux = spectrum.flux.value
     disp = spectrum.dispersion.value
     meta = spectrum.meta
