2faa288fb22e19a10adf72f5766fee4c3fa429f2
nmearl <contact@nicholasearl.me>
2017-04-26 12:01:47 -0400

    Groundwork for wcs adapter

---
+++ specutils/adapters/wcs.py
@@ -0,0 +1,53 @@
+import six
+import abc
+from astropy.wcs import WCS
+
+
+class WCSAdapterMeta(type):
+    def __new__(cls, name, parents, dct):
+        # create a class_id if it's not specified
+        if 'class_id' not in dct:
+            dct['class_id'] = name.lower()
+
+        # open the specified file for writing
+        if 'file' in dct:
+            filename = dct['file']
+            dct['file'] = open(filename, 'w')
+
+        # we need to call type.__new__ to complete the initialization
+        return super(WCSAdapterMeta, cls).__new__(cls, name, parents, dct)
+
+    def __init__(cls, name, bases, dct):
+        if not hasattr(cls, 'registry'):
+            cls.registry = {}
+        else:
+            wcs_class = dct.get('wcs', None)
+
+            if wcs_class is None:
+                raise NotImplemented
+
+            cls.registry.get(wcs_class, []).append(cls)
+
+        super(WCSAdapterMeta, cls).__init__(name, bases, dct)
+
+
+class WCSAdapterMetaProxy(abc.ABCMeta, WCSAdapterMeta):
+    pass
+
+
+@six.add_metaclass(WCSAdapterMetaProxy)
+class WCSAdapter:
+    def __init__(self, wcs):
+        pass
+
+    @abc.abstractmethod
+    def world_to_pix(self):
+        pass
+
+    @abc.abstractmethod
+    def pix_to_world(self):
+        pass
+
+
+class FITSWCSAdapter:
+    wcs = WCS
