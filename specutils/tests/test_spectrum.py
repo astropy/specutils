706de5c6c61dcfa8ae90cecfaec7e45183ae9e41
Brigitta Sipocz <bsipocz@gmail.com>
2017-05-26 13:15:05 +0100

    Fixing file format for readin

--- specutils/tests/test_spectrum.py
+++ specutils/tests/test_spectrum.py
@@ -9,10 +9,11 @@ def test_spectrum1d_simple():
 
 def test_spectrum1d_GMOSfits():
     optical_fits_file = get_pkg_data_filename('data/L5g_0355+11_Cruz09.fits')
-    optical_spec_2 = Spectrum1D.read(optical_fits_file)
+    optical_spec_2 = Spectrum1D.read(optical_fits_file, format='wcs1d-fits')
     assert len(optical_spec_2.data) == 3020
 
 def test_specific_spec_axis_unit():
     optical_fits_file = get_pkg_data_filename('data/L5g_0355+11_Cruz09.fits')
-    optical_spec =  Spectrum1D.read(optical_fits_file, spectral_axis_unit="Angstrom")
+    optical_spec =  Spectrum1D.read(optical_fits_file, spectral_axis_unit="Angstrom",
+                                    format='wcs1d-fits')
     assert optical_spec.spectral_axis.unit == "Angstrom"
