1ea6440e8b8aaeecaff3e3346ae87c1152f010b9
Brigitta Sipocz <bsipocz@gmail.com>
2017-05-26 13:07:55 +0100

    Making CI work by adding datafile and module, and two temporary workarounds:  - Removing doctesting example for mock method spectral_resolution  - Renaming spectral-cube test file that was copied over verbatim, this need to be removed or functionality being implemented first

--- specutils/tests/setup_package.py
+++ specutils/tests/setup_package.py
@@ -1,3 +1,4 @@
 def get_package_data():
     return {
-        _ASTROPY_PACKAGE_NAME_ + '.tests': ['coveragerc']}
+        _ASTROPY_PACKAGE_NAME_ + '.tests': ['coveragerc'],
+        'specutils.tests': ['data/*fits']}
