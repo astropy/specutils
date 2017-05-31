328c466752a13e9b2b666e485441fda50a2d97f4
Brigitta Sipocz <bsipocz@gmail.com>
2017-05-31 15:02:51 +0100

    xfailing test until the datafile gets updated

--- specutils/tests/test_spectrum.py
+++ specutils/tests/test_spectrum.py
@@ -1,5 +1,6 @@
 from __future__ import absolute_import, division
 from astropy.utils.data import get_pkg_data_filename
+import pytest
 
 from ..spectra.spectrum1d import Spectrum1D
 
@@ -12,6 +13,9 @@ def test_spectrum1d_GMOSfits():
     optical_spec_2 = Spectrum1D.read(optical_fits_file, format='wcs1d-fits')
     assert len(optical_spec_2.data) == 3020
 
+
+@pytest.mark.xfail(reason='The file L5g_0355+11_Cruz09 does not have a '
+                   'spectral axis in its WCS')
 def test_specific_spec_axis_unit():
     optical_fits_file = get_pkg_data_filename('data/L5g_0355+11_Cruz09.fits')
     optical_spec =  Spectrum1D.read(optical_fits_file, spectral_axis_unit="Angstrom",
