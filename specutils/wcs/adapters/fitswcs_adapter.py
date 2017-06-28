7f8e1daf971b07797d8b37da3e19c86b7f1bf5ea
nmearl <nchlsearl@gmail.com>
2017-06-28 13:14:54 -0400

    Wrap axes in adapter class; include bin edges

--- specutils/wcs/adapters/fitswcs_adapter.py
+++ specutils/wcs/adapters/fitswcs_adapter.py
@@ -16,12 +16,12 @@ class FITSWCSAdapter(WCSAdapter):
         super(FITSWCSAdapter, self).__init__(wcs)
 
         self.axes = WCSAxes(
-            longitude=self.wcs.sub([WCSSUB_LONGITUDE]),
-            latitude=self.wcs.sub([WCSSUB_LATITUDE]),
-            cubeface=self.wcs.sub([WCSSUB_CUBEFACE]),
-            spectral=self.wcs.sub([WCSSUB_SPECTRAL]),
-            stokes=self.wcs.sub([WCSSUB_STOKES]),
-            celestial=self.wcs.sub([WCSSUB_CELESTIAL])
+            longitude=FITSWCSAdapter(self.wcs.sub([WCSSUB_LONGITUDE])),
+            latitude=FITSWCSAdapter(self.wcs.sub([WCSSUB_LATITUDE])),
+            cubeface=FITSWCSAdapter(self.wcs.sub([WCSSUB_CUBEFACE])),
+            spectral=FITSWCSAdapter(self.wcs.sub([WCSSUB_SPECTRAL])),
+            stokes=FITSWCSAdapter(self.wcs.sub([WCSSUB_STOKES])),
+            celestial=FITSWCSAdapter(self.wcs.sub([WCSSUB_CELESTIAL]))
         )
 
     def world_to_pix(self):
@@ -29,3 +29,10 @@ class FITSWCSAdapter(WCSAdapter):
 
     def pix_to_world(self):
         pass
+
+    def bin_edges(self):
+        # the WCS doesn't know about its own pixel array
+        edge_indices = list(self.wcs.pixel_indices-0.5) + \
+                       [self.wcs.pixel_indices[-1]+0.5]
+
+        return self.pix2world(edge_indices, 0)
