c943a0c31e5b7b0bd292f17821b1bd7ccbfcd50d
nmearl <contact@nicholasearl.me>
2018-02-06 14:53:22 -0500

    Add method for generating a default WCS when no WCS object is given

--- specutils/wcs/wcs_wrapper.py
+++ specutils/wcs/wcs_wrapper.py
@@ -1,6 +1,11 @@
 from .wcs_adapter import WCSAdapter
 from .adapters import *
 
+import gwcs
+from gwcs import coordinate_frames as cf
+from astropy.modeling.tabular import Tabular1D
+from astropy.units import quantity_input
+import numpy as np
 
 __all__ = ['WCSWrapper']
 
@@ -10,6 +15,7 @@ class WCSWrapper:
     Factory class that returns an instance of a registered WCS adapter
     dependent upon the class of the `wcs` object passed in.
     """
+
     def __new__(cls, wcs, *args, **kwargs):
         """
         Control the creation of a new instance of a ~`WCSAdapter` subclass.
@@ -36,3 +42,21 @@ class WCSWrapper:
 
         raise NotImplementedError("No such adapter for class "
                                   "{}".format(wcs.__class__))
+
+    @staticmethod
+    @quantity_input(array=['length', 'frequency', 'speed'])
+    def from_array(array):
+        """
+        Create a new WCS from provided tabular data. This defaults to being
+        a GWCS object.
+        """
+        coord_frame = cf.CoordinateFrame(naxes=1,
+                                         axes_type=('SPECTRAL',),
+                                         axes_order=(0,))
+        spec_frame = cf.SpectralFrame(unit=array.unit, axes_order=(0,))
+        transform = Tabular1D(np.arange(len(array)), array.value)
+
+        tabular_gwcs = gwcs.wcs.WCS(
+            [(coord_frame, transform), (spec_frame, None)])
+
+        return WCSWrapper(wcs=tabular_gwcs)
