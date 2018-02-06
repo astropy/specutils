b07289f5dc542736acba69288f6e7ab89e224d88
nmearl <contact@nicholasearl.me>
2018-02-06 14:54:23 -0500

    Update GWCS adapter to be functional (for forward operations currently)

--- specutils/wcs/adapters/gwcs_adapter.py
+++ specutils/wcs/adapters/gwcs_adapter.py
@@ -6,18 +6,44 @@ __all__ = ['GWCSAdapter']
 
 
 class GWCSAdapter(WCSAdapter):
+    """
+    Adapter class that adds support for GWCS objects.
+    """
     wrapped_class = WCS
     axes = None
 
     def __init__(self, wcs):
         super(GWCSAdapter, self).__init__(wcs)
 
-        self.axes = WCSAxes(
-            spectral=self.wcs.forward_transform | models.Mapping((2,))
-        )
+    def world_to_pixel(self, world_array):
+        """
+        Method for performing the world to pixel transformations.
+        """
+        return self.wcs.backward_transform(world_array)
 
-    def world_to_pix(self):
-        pass
+    def pixel_to_world(self, pixel_array):
+        """
+        Method for performing the pixel to world transformations.
+        """
+        return self.wcs(pixel_array, with_bounding_box=False) * self.spectral_axis_unit
 
-    def pix_to_world(self):
-        pass
+    @property
+    def spectral_axis_unit(self):
+        """
+        Returns the unit of the spectral axis.
+        """
+        return self._wcs.output_frame.unit[0]
+
+    @property
+    def rest_frequency(self):
+        """
+        Returns the rest frequency defined in the WCS.
+        """
+        return
+
+    @property
+    def rest_wavelength(self):
+        """
+        Returns the rest wavelength defined in the WCS.
+        """
+        return
