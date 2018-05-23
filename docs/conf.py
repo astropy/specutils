2c8158b7aaa22b7dfba15e1663e98fd44ce1bfba
Erik Tollerud <erik.tollerud@gmail.com>
2018-05-22 23:56:42 -0400

    change to specutils text

--- docs/conf.py
+++ docs/conf.py
@@ -114,8 +114,8 @@ release = package.__version__
 
 # Please update these texts to match the name of your package.
 html_theme_options = {
-    'logotext1': 'package',  # white,  semi-bold
-    'logotext2': '-template',  # orange, light
+    'logotext1': 'spec',  # white,  semi-bold
+    'logotext2': 'utils',  # orange, light
     'logotext3': ':docs'   # white,  light
     }
 
