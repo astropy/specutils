2187a80a106bf7b7c96c2fb378fe9032da520cf3
brechmos <craig@brechmos.org>
2018-05-24 16:03:47 -0400

    convolution and median smoothing of flux

--- specutils/processing/smoothing.py
+++ specutils/processing/smoothing.py
@@ -1,32 +1,48 @@
 from __future__ import division
 
 from astropy import convolution
-
+from scipy.signal import medfilt
+from ..spectra import Spectrum1D
 
 __all__ = ['convolution_smooth', 'box_smooth', 'gaussian_smooth',
-           'mexicanhat_smooth', 'trapezoid_smooth']
+           'mexicanhat_smooth', 'trapezoid_smooth', 'median_smooth']
 
 
 def convolution_smooth(spectrum, kernel, inplace=False):
     """
-    Does a naive equivalent width measures on the spectrum object.
+    Apply a convolution based smoothing to the spectrum. The kernel must be one
+    of the 1D kernels defined in ``astropy.convolution``.
+
+    This method can be used along but also is used by other specific methods below.
 
     Parameters
     ----------
-    spectrum : Spectrum1D
-        The spectrum object overwhich the equivalent width will be calculated.
-    kernel : astropy.modeling.models.Fittable1DModel
+    spectrum : `~specutils.spectra.Spectrum1D`
+        The `~specutils.spectra.Spectrum1D` object to which the smoothing will be applied.
+    kernel : ``astropy.convolution.Kernel1D`` subclass.
         The convolution based smoothing kernel.
     inplace : bool
         Choose if the smoothing should be applied to the input spectrum or a copy.
+        Default is False.
 
     Returns
     -------
-    spectrum : spectrum1D
-        Equivalent width calculation.
+    spectrum : `~specutils.spectra.Spectrum1D`
+        Output `~specutils.spectra.Spectrum1D` which is either the one passed in (inplace=True) or
+        a copy of the one passed in with the updated flux (inplace=False)
 
-    TODO:  what frame of reference do you want the spectral_axis to be in ???
+    Raises
+    ------
+    ValueError
+       In the case that ``spectrum`` and ``kernel`` are not the correct types.
+
     """
+    # Parameter checks
+    if not isinstance(spectrum, Spectrum1D):
+        raise ValueError('The spectrum parameter must be a Spectrum1D object')
+
+    if not isinstance(kernel, convolution.Kernel1D):
+        raise ValueError('The kernel parameter must be a subclass of the astropy.convolution.Kernel1D')
 
     # Get the flux of the input spectrum
     flux = spectrum.flux
@@ -47,24 +63,37 @@ def convolution_smooth(spectrum, kernel, inplace=False):
 
 def box_smooth(spectrum, width, inplace=False):
     """
-    Smoothing based on a Gaussian kernel.
+    Smooth a `~specutils.spectra.Spectrum1D` instance based on a ``astropy.convolution.Box1D`` kernel.
 
     Parameters
     ----------
-    spectrum : Spectrum1D
-        The spectrum object overwhich the equivalent width will be calculated.
+    spectrum : `~specutils.spectra.Spectrum1D`
+        The spectrum object to which the smoothing will be applied.
     width : number
-        The width of the kernel as defined in astropy.convolution.Box1DKernel
+        The width of the kernel as defined in ``astropy.convolution.Box1DKernel``
     inplace : bool
         Choose if the smoothing should be applied to the input spectrum or a copy.
+        Default is False.
 
     Returns
     -------
-    spectrum : spectrum1D
-        Equivalent width calculation.
+    spectrum : `~specutils.spectra.Spectrum1D`
+        Output `~specutils.spectra.Spectrum1D` which is either the one passed in (inplace=True) or
+        a copy of the one passed in with the updated flux (inplace=False)
+
+    Raises
+    ------
+    ValueError
+       In the case that ``width`` is not the correct type or value.
+
     """
+    # Parameter checks
+    if not isinstance(width, (int, float)) or width <= 0:
+        raise ValueError('The width parameter, {}, must be a number greater than 0'.format(
+                width))
+
     # Create the gaussian kernel
-    box1d_kernel = convolution.Box1D(width)
+    box1d_kernel = convolution.Box1DKernel(width)
 
     # Call and return the convolution smoothing.
     return convolution_smooth(spectrum, box1d_kernel, inplace)
@@ -72,24 +101,37 @@ def box_smooth(spectrum, width, inplace=False):
 
 def gaussian_smooth(spectrum, stddev, inplace=False):
     """
-    Smoothing based on a Gaussian kernel.
+    Smooth a `~specutils.spectra.Spectrum1D` instance based on a ``astropy.convolution.Gaussian1DKernel``.
 
     Parameters
     ----------
-    spectrum : Spectrum1D
-        The spectrum object overwhich the equivalent width will be calculated.
+    spectrum : `~specutils.spectra.Spectrum1D`
+        The spectrum object to which the smoothing will be applied.
     stddev : number
-        The stddev of the kernel as defined in astropy.convolution.Gaussian1DKernel
+        The stddev of the kernel as defined in ``astropy.convolution.Gaussian1DKernel``
     inplace : bool
         Choose if the smoothing should be applied to the input spectrum or a copy.
+        Default is False.
 
     Returns
     -------
-    spectrum : spectrum1D
-        Equivalent width calculation.
+    spectrum : `~specutils.spectra.Spectrum1D`
+        Output `~specutils.spectra.Spectrum1D` which is either the one passed in (inplace=True) or
+        a copy of the one passed in with the updated flux (inplace=False)
+
+    Raises
+    ------
+    ValueError
+       In the case that ``stddev`` is not the correct type or value.
+
     """
+    # Parameter checks
+    if not isinstance(stddev, (int, float)) or stddev <= 0:
+        raise ValueError('The stddev parameter, {}, must be a number greater than 0'.format(
+                stddev))
+
     # Create the gaussian kernel
-    gaussian_kernel = convolution.Gaussian1D(stddev)
+    gaussian_kernel = convolution.Gaussian1DKernel(stddev)
 
     # Call and return the convolution smoothing.
     return convolution_smooth(spectrum, gaussian_kernel, inplace)
@@ -97,24 +139,37 @@ def gaussian_smooth(spectrum, stddev, inplace=False):
 
 def mexicanhat_smooth(spectrum, width, inplace=False):
     """
-    Smoothing based on a Gaussian kernel.
+    Smoothing based on an ``astropy.convolution.MexicanHat1DKernel``.
 
     Parameters
     ----------
-    spectrum : Spectrum1D
-        The spectrum object overwhich the equivalent width will be calculated.
+    spectrum : `~specutils.spectra.Spectrum1D`
+        The `~specutils.spectra.Spectrum1D` object to which the smoothing will be applied.
     width : number
-        The width of the kernel as defined in astropy.convolution.MexicanHat1DKernel
+        The width of the kernel as defined in ``astropy.convolution.MexicanHat1DKernel``
     inplace : bool
         Choose if the smoothing should be applied to the input spectrum or a copy.
+        Default is False.
 
     Returns
     -------
-    spectrum : spectrum1D
-        Equivalent width calculation.
+    spectrum : `~specutils.spectra.Spectrum1D`
+        Output `~specutils.spectra.Spectrum1D` which is either the one passed in (inplace=True) or
+        a copy of the one passed in with the updated flux (inplace=False)
+
+    Raises
+    ------
+    ValueError
+       In the case that ``width`` is not the correct type or value.
+
     """
+    # Parameter checks
+    if not isinstance(width, (int, float)) or width <= 0:
+        raise ValueError('The stddev parameter, {}, must be a number greater than 0'.format(
+                width))
+
     # Create the gaussian kernel
-    mexicanhat_kernel = convolution.MexicanHat1D(width)
+    mexicanhat_kernel = convolution.MexicanHat1DKernel(width)
 
     # Call and return the convolution smoothing.
     return convolution_smooth(spectrum, mexicanhat_kernel, inplace)
@@ -122,24 +177,89 @@ def mexicanhat_smooth(spectrum, width, inplace=False):
 
 def trapezoid_smooth(spectrum, width, inplace=False):
     """
-    Smoothing based on a Gaussian kernel.
+    Smoothing based on a Trapezoid kernel.
 
     Parameters
     ----------
-    spectrum : Spectrum1D
-        The spectrum object overwhich the equivalent width will be calculated.
+    spectrum : `~specutils.spectra.Spectrum1D`
+        The `~specutils.spectra.Spectrum1D` object to which the smoothing will be applied.
     width : number
-        The width of the kernel as defined in astropy.convolution.Trapezoid1DKernel
+        The width of the kernel as defined in ``astropy.convolution.Trapezoid1DKernel``
     inplace : bool
         Choose if the smoothing should be applied to the input spectrum or a copy.
+        Default is False.
 
     Returns
     -------
-    spectrum : spectrum1D
-        Equivalent width calculation.
+    spectrum : `~specutils.spectra.Spectrum1D`
+        Output `~specutils.spectra.Spectrum1D` which is either the one passed in (inplace=True) or
+        a copy of the one passed in with the updated flux (inplace=False)
+
+    Raises
+    ------
+    ValueError
+       In the case that ``width`` is not the correct type or value.
+
     """
+    # Parameter checks
+    if not isinstance(width, (int, float)) or width <= 0:
+        raise ValueError('The stddev parameter, {}, must be a number greater than 0'.format(
+                width))
+
     # Create the gaussian kernel
-    trapezoid_kernel = convolution.Trapezoid1D(width)
+    trapezoid_kernel = convolution.Trapezoid1DKernel(width)
 
     # Call and return the convolution smoothing.
     return convolution_smooth(spectrum, trapezoid_kernel, inplace)
+
+
+def median_smooth(spectrum, width, inplace=False):
+    """
+    Smoothing based on a median filter. The median filter smoothing
+    is implemented using the ``scipy.signals.medfilt`` function.
+
+    Parameters
+    ----------
+    spectrum : `~specutils.spectra.Spectrum1D`
+        The `~specutils.spectra.Spectrum1D` object to which the smoothing will be applied.
+    width : number
+        The width of the median filter.
+    inplace : bool
+        Choose if the smoothing should be applied to the input spectrum or a copy.
+        Default is False.
+
+    Returns
+    -------
+    spectrum : `~specutils.spectra.Spectrum1D`
+        Output `~specutils.spectra.Spectrum1D` which is either the one passed in (inplace=True) or
+        a copy of the one passed in with the updated flux (inplace=False)
+
+    Raises
+    ------
+    ValueError
+       In the case that ``spectrum`` or ``width`` are not the correct type or value.
+
+    """
+    # Parameter checks
+    if not isinstance(spectrum, Spectrum1D):
+        raise ValueError('The spectrum parameter must be a Spectrum1D object')
+
+    if not isinstance(width, (int, float)) or width <= 0:
+        raise ValueError('The stddev parameter, {}, must be a number greater than 0'.format(
+                width))
+
+    # Get the flux of the input spectrum
+    flux = spectrum.flux
+
+    # Smooth based on the input kernel
+    smoothed_flux = medfilt(flux, width)
+
+    # Set the smoothed flux to the input spectrum (inplace) or
+    # to a copy and return (not inplace)
+    if inplace:
+        spectrum.flux = smoothed_flux
+        return spectrum
+    else:
+        spectrum_out = spectrum.copy()
+        spectrum_out.flux = smoothed_flux
+        return spectrum_out
--- specutils/tests/test_smoothing.py
+++ specutils/tests/test_smoothing.py
@@ -1,20 +1,23 @@
 import numpy as np
+import pytest
 from astropy import convolution
+from scipy.signal import medfilt
 import astropy.units as u
 from ..spectra.spectrum1d import Spectrum1D
 
 from ..processing.smoothing import (box_smooth, gaussian_smooth,
-                                    mexicanhat_smooth, trapezoid_smooth)
+                                    mexicanhat_smooth, trapezoid_smooth,
+                                    median_smooth)
 
 
-def test_smooth_box():
+@pytest.mark.parameterize(width=[1, 2.3])
+def test_smooth_box_good(width):
 
     #  Create the spectrum
     spec1 = Spectrum1D(spectral_axis=np.arange(1, 50) * u.nm,
                        flux=np.random.sample(49) * 100)
 
     flux = spec1.flux
-    width = 3
 
     # Create the flux_smoothed which is what we want to compare to
     box_kernel = convolution.Gaussian1D(width)
@@ -33,14 +36,26 @@ def test_smooth_box():
     assert np.allclose(spec1_smoothed, flux_smoothed)
 
 
-def test_smooth_gaussian():
+@pytest.mark.parameterize(width=[-1, 'a'])
+def test_smooth_box_bad(width):
+
+    #  Create the spectrum
+    spec1 = Spectrum1D(spectral_axis=np.arange(1, 50) * u.nm,
+                       flux=np.random.sample(49) * 100)
+
+    # Test not-in-place smoothing, defualt is not in-place
+    with pytest.raises(ValueError):
+        box_smooth(spec1, width)
+
+
+@pytest.mark.parameterize(stddev=[1, 2.3])
+def test_smooth_gaussian_good(stddev):
 
     #  Create the spectrum
     spec1 = Spectrum1D(spectral_axis=np.arange(1, 50) * u.nm,
                        flux=np.random.sample(49) * 100)
 
     flux = spec1.flux
-    stddev = 3
 
     # Create the flux_smoothed which is what we want to compare to
     gaussian_kernel = convolution.Gaussian1D(stddev)
@@ -58,15 +73,26 @@ def test_smooth_gaussian():
     spec1_smoothed = gaussian_smooth(spec1, stddev, inplace=True)
     assert np.allclose(spec1_smoothed, flux_smoothed)
 
+@pytest.mark.parameterize(stddev=[-1, 'a'])
+def test_smooth_gaussian_bad(stddev):
 
-def test_smooth_mexicanhat():
+    #  Create the spectrum
+    spec1 = Spectrum1D(spectral_axis=np.arange(1, 50) * u.nm,
+                       flux=np.random.sample(49) * 100)
+
+    # Test not-in-place smoothing, defualt is not in-place
+    with pytest.raises(ValueError):
+        gaussian_smooth(spec1, stddev)
+
+
+@pytest.mark.parameterize(stddev=[1, 2.3])
+def test_smooth_mexicanhat_good(stddev):
 
     #  Create the spectrum
     spec1 = Spectrum1D(spectral_axis=np.arange(1, 50) * u.nm,
                        flux=np.random.sample(49) * 100)
 
     flux = spec1.flux
-    stddev = 3
 
     # Create the flux_smoothed which is what we want to compare to
     mexicanhat_kernel = convolution.MexicanHat1D(stddev)
@@ -85,14 +111,26 @@ def test_smooth_mexicanhat():
     assert np.allclose(spec1_smoothed, flux_smoothed)
 
 
-def test_smooth_trapezoid():
+@pytest.mark.parameterize(stddev=[-1, 'a'])
+def test_smooth_mexicanhat_bad(stddev):
+
+    #  Create the spectrum
+    spec1 = Spectrum1D(spectral_axis=np.arange(1, 50) * u.nm,
+                       flux=np.random.sample(49) * 100)
+
+    # Test not-in-place smoothing, defualt is not in-place
+    with pytest.raises(ValueError):
+        mexicanhat_smooth(spec1, stddev)
+
+
+@pytest.mark.parameterize(stddev=[1, 2.3])
+def test_smooth_trapezoid_good(stddev):
 
     #  Create the spectrum
     spec1 = Spectrum1D(spectral_axis=np.arange(1, 50) * u.nm,
                        flux=np.random.sample(49) * 100)
 
     flux = spec1.flux
-    stddev = 3
 
     # Create the flux_smoothed which is what we want to compare to
     trapezoid_kernel = convolution.Trapezoid1D(stddev)
@@ -109,3 +147,52 @@ def test_smooth_trapezoid():
     # Test in-place smoothing
     spec1_smoothed = trapezoid_smooth(spec1, stddev, inplace=True)
     assert np.allclose(spec1_smoothed, flux_smoothed)
+
+
+@pytest.mark.parameterize(stddev=[-1, 'a'])
+def test_smooth_trapezoid_bad(stddev):
+
+    #  Create the spectrum
+    spec1 = Spectrum1D(spectral_axis=np.arange(1, 50) * u.nm,
+                       flux=np.random.sample(49) * 100)
+
+    # Test not-in-place smoothing, defualt is not in-place
+    with pytest.raises(ValueError):
+        trapezoid_smooth(spec1, stddev)
+
+
+@pytest.mark.parameterize(width=[1, 2.3])
+def test_smooth_median_good(width):
+
+    #  Create the spectrum
+    spec1 = Spectrum1D(spectral_axis=np.arange(1, 50) * u.nm,
+                       flux=np.random.sample(49) * 100)
+
+    flux = spec1.flux
+
+    # Create the flux_smoothed which is what we want to compare to
+    flux_smoothed = medfilt(flux, width)
+
+    # Test not-in-place smoothing, defualt is not in-place
+    spec1_smoothed = median_smooth(spec1, width)
+    assert np.allclose(spec1_smoothed, flux_smoothed)
+
+    # Test not-in-place smoothing
+    spec1_smoothed = median_smooth(spec1, width, inplace=False)
+    assert np.allclose(spec1_smoothed, flux_smoothed)
+
+    # Test in-place smoothing
+    spec1_smoothed = median_smooth(spec1, width, inplace=True)
+    assert np.allclose(spec1_smoothed, flux_smoothed)
+
+
+@pytest.mark.parameterize(width=[-1, 'a'])
+def test_smooth_median_bad(width):
+
+    #  Create the spectrum
+    spec1 = Spectrum1D(spectral_axis=np.arange(1, 50) * u.nm,
+                       flux=np.random.sample(49) * 100)
+
+    # Test not-in-place smoothing, defualt is not in-place
+    with pytest.raises(ValueError):
+        median_smooth(spec1, width)
