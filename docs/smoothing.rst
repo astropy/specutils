d15f2c7955fecef14ac147a71c294c9b4d9bfadd
brechmos <craig@brechmos.org>
2018-05-29 15:02:07 -0400

    removed processing directory strucutre, now just smoothing

--- docs/smoothing.rst
+++ docs/smoothing.rst
@@ -3,24 +3,18 @@ Spectral Smoothing
 ==================
 
 The spectral smoothing has two forms, 1) convolution based smoothing 
-using the ``astropy.convolution`` package and 2) median filter
-using the :func:``scipy.signal.medfilt``.  Each of these act on the flux
+using the `astropy.convolution` package and 2) median filter
+using the :func:`scipy.signal.medfilt`.  Each of these act on the flux
 of the :class:`~specutils.spectra.Spectram1D` object.
 
 Convolution Based Smoothing
 ---------------------------
 
-The convolution based smoothing supports smoothing based on 1D kernels in 
-``astropy.convlution.convolve``.  Currently implemented are 
-:func:`~specutils.processing.smoothing.box_smooth` (:class:``astropy.convolution.convolve.Box1DKernel``),  
-:func:`~specutils.processing.smoothing.gaussian_smooth` (:class:``astropy.convolution.convolve.Gaussian1DKernel``),  
-and :func:`~specutils.processing.smoothing.trapzoid_smooth` (:class:``astropy.convolution.convolve.Trapezoid1DKernel``).
+While any kernel supported by `astropy.convolution` will work (using the `convolution_smooth` function), several commonly-used kernels have convenience functions wrapping them to simplify the smoothing process into a simple one-line operation.  Currently implemented are:
+:func:`~specutils.processing.smoothing.box_smooth` (:class:`astropy.convolution.convolve.Box1DKernel`),  
+:func:`~specutils.processing.smoothing.gaussian_smooth` (:class:`astropy.convolution.convolve.Gaussian1DKernel`),  
+and :func:`~specutils.processing.smoothing.trapzoid_smooth` (:class:`astropy.convolution.convolve.Trapezoid1DKernel`).
 
-Each of the functions also has a parameter ``inplace`` which takes a boolean 
-to tell the method to either apply the filter to the flux on the input object
-(``inplace=True``) or to filter the flux on a copy of the input object (``inplace=False``).
-In the latter case, the returned spectrum is a copy of the input spectrum but with the
-smoothed flux.
 
 .. code-block:: python
 
@@ -34,7 +28,7 @@ smoothed flux.
     >>> spec1_gsmooth = gaussian_smooth(spec1, stddev=3)
     >>> spec1_tsmooth = trapezoid_smooth(spec1, width=3)
 
-    >>> gaussian_smooth(spec1, stddev=3, inplace=True)
+    >>> gaussian_smooth(spec1, stddev=3)
     Spectrum1D([0.22830748, 0.2783204 , 0.32007408, 0.35270403, 0.37899655,
                 0.40347983, 0.42974259, 0.45873436, 0.48875214, 0.51675647,
                 0.54007149, 0.55764758, 0.57052796, 0.58157173, 0.59448669,
@@ -47,7 +41,7 @@ smoothed flux.
                 0.50453064, 0.46677128, 0.41125485, 0.34213489])
 
 
-Each of the specific smoothing methods create the appropriate ``astropy.convolution.convolve`` 
+Each of the specific smoothing methods create the appropriate `astropy.convolution.convolve` 
 kernel and then call a helper function :func:`~specutils.processing.smoothing.convolution_smooth` 
 that takes the spectrum and an astropy 1D kernel.  So, one could also do:
 
@@ -65,13 +59,13 @@ that takes the spectrum and an astropy 1D kernel.  So, one could also do:
     >>> spec1_bsmooth2 = convolution_smooth(spec1, box1d_kernel)
 
 In this case, the ``spec1_bsmooth2`` result should be equivalent to the ``spec1_bsmooth`` in
-the section above (assuming the flux data of the input `spec` is the same).
+the section above (assuming the flux data of the input ``spec`` is the same).
 
 
 Median Smoothing
 ----------------
 
-The median based smoothing  is implemented using ``scipy.signal.medfilt`` and
+The median based smoothing  is implemented using `scipy.signal.medfilt` and
 has a similar call structure to the convolution-based smoothing methods. This 
 method applys the median filter across the flux.
 
---
+++ specutils/smoothing/smoothing.py
@@ -0,0 +1,204 @@
+from __future__ import division
+
+# TODO: This can be removed when specutils has a deepcopy method.
+from copy import deepcopy
+
+from astropy import convolution
+from scipy.signal import medfilt
+from ..spectra import Spectrum1D
+
+__all__ = ['convolution_smooth', 'box_smooth', 'gaussian_smooth',
+           'trapezoid_smooth', 'median_smooth']
+
+
+def convolution_smooth(spectrum, kernel):
+    """
+    Apply a convolution based smoothing to the spectrum. The kernel must be one
+    of the 1D kernels defined in `astropy.convolution`.
+
+    This method can be used along but also is used by other specific methods below.
+
+    Parameters
+    ----------
+    spectrum : `~specutils.spectra.Spectrum1D`
+        The `~specutils.spectra.Spectrum1D` object to which the smoothing will be applied.
+    kernel : `astropy.convolution.Kernel1D` subclass.
+        The convolution based smoothing kernel.
+
+    Returns
+    -------
+    spectrum : `~specutils.spectra.Spectrum1D`
+        Output `~specutils.spectra.Spectrum1D` which is copy of the one passed in with the updated flux.
+
+    Raises
+    ------
+    ValueError
+       In the case that ``spectrum`` and ``kernel`` are not the correct types.
+
+    """
+    # Parameter checks
+    if not isinstance(spectrum, Spectrum1D):
+        raise ValueError('The spectrum parameter must be a Spectrum1D object')
+
+    if not isinstance(kernel, convolution.Kernel1D):
+        raise ValueError('The kernel parameter must be a subclass of the astropy.convolution.Kernel1D')
+
+    # Get the flux of the input spectrum
+    flux = spectrum.flux
+
+    # Smooth based on the input kernel
+    smoothed_flux = convolution.convolve(flux, kernel)
+
+    # Return a new object with the smoothed flux.
+    return Spectrum1D(flux=smoothed_flux, spectral_axis=spectrum.spectral_axis,
+                      wcs=spectrum.wcs, unit=spectrum.unit, 
+                      spectral_axis_unit=spectrum.spectral_axis_unit, 
+                      velocity_convention=spectrum.velocity_convention,
+                      rest_value=spectrum.rest_value)
+
+
+def box_smooth(spectrum, width):
+    """
+    Smooth a `~specutils.spectra.Spectrum1D` instance based on a `astropy.convolution.Box1D` kernel.
+
+    Parameters
+    ----------
+    spectrum : `~specutils.spectra.Spectrum1D`
+        The spectrum object to which the smoothing will be applied.
+    width : number
+        The width of the kernel as defined in `astropy.convolution.Box1DKernel`
+
+    Returns
+    -------
+    spectrum : `~specutils.spectra.Spectrum1D`
+        Output `~specutils.spectra.Spectrum1D` which a copy of the one passed in with the updated flux.
+
+    Raises
+    ------
+    ValueError
+       In the case that ``width`` is not the correct type or value.
+
+    """
+    # Parameter checks
+    if not isinstance(width, (int, float)) or width <= 0:
+        raise ValueError('The width parameter, {}, must be a number greater than 0'.format(
+                width))
+
+    # Create the gaussian kernel
+    box1d_kernel = convolution.Box1DKernel(width)
+
+    # Call and return the convolution smoothing.
+    return convolution_smooth(spectrum, box1d_kernel)
+
+
+def gaussian_smooth(spectrum, stddev):
+    """
+    Smooth a `~specutils.spectra.Spectrum1D` instance based on a `astropy.convolution.Gaussian1DKernel`.
+
+    Parameters
+    ----------
+    spectrum : `~specutils.spectra.Spectrum1D`
+        The spectrum object to which the smoothing will be applied.
+    stddev : number
+        The stddev of the kernel as defined in `astropy.convolution.Gaussian1DKernel`
+
+    Returns
+    -------
+    spectrum : `~specutils.spectra.Spectrum1D`
+        Output `~specutils.spectra.Spectrum1D` which is copy of the one passed in with the updated flux.
+
+    Raises
+    ------
+    ValueError
+       In the case that ``stddev`` is not the correct type or value.
+
+    """
+    # Parameter checks
+    if not isinstance(stddev, (int, float)) or stddev <= 0:
+        raise ValueError('The stddev parameter, {}, must be a number greater than 0'.format(
+                stddev))
+
+    # Create the gaussian kernel
+    gaussian_kernel = convolution.Gaussian1DKernel(stddev)
+
+    # Call and return the convolution smoothing.
+    return convolution_smooth(spectrum, gaussian_kernel)
+
+
+def trapezoid_smooth(spectrum, width):
+    """
+    Smoothing based on a Trapezoid kernel.
+
+    Parameters
+    ----------
+    spectrum : `~specutils.spectra.Spectrum1D`
+        The `~specutils.spectra.Spectrum1D` object to which the smoothing will be applied.
+    width : number
+        The width of the kernel as defined in `astropy.convolution.Trapezoid1DKernel`
+
+    Returns
+    -------
+    spectrum : `~specutils.spectra.Spectrum1D`
+        Output `~specutils.spectra.Spectrum1D` which is copy of the one passed in with the updated flux.
+
+    Raises
+    ------
+    ValueError
+       In the case that ``width`` is not the correct type or value.
+
+    """
+    # Parameter checks
+    if not isinstance(width, (int, float)) or width <= 0:
+        raise ValueError('The stddev parameter, {}, must be a number greater than 0'.format(
+                width))
+
+    # Create the gaussian kernel
+    trapezoid_kernel = convolution.Trapezoid1DKernel(width)
+
+    # Call and return the convolution smoothing.
+    return convolution_smooth(spectrum, trapezoid_kernel)
+
+
+def median_smooth(spectrum, width):
+    """
+    Smoothing based on a median filter. The median filter smoothing
+    is implemented using the `scipy.signals.medfilt` function.
+
+    Parameters
+    ----------
+    spectrum : `~specutils.spectra.Spectrum1D`
+        The `~specutils.spectra.Spectrum1D` object to which the smoothing will be applied.
+    width : number
+        The width of the median filter.
+
+    Returns
+    -------
+    spectrum : `~specutils.spectra.Spectrum1D`
+        Output `~specutils.spectra.Spectrum1D` which is copy of the one passed in with the updated flux.
+
+    Raises
+    ------
+    ValueError
+       In the case that ``spectrum`` or ``width`` are not the correct type or value.
+
+    """
+    # Parameter checks
+    if not isinstance(spectrum, Spectrum1D):
+        raise ValueError('The spectrum parameter must be a Spectrum1D object')
+
+    if not isinstance(width, (int, float)) or width <= 0:
+        raise ValueError('The stddev parameter, {}, must be a number greater than 0'.format(
+                width))
+
+    # Get the flux of the input spectrum
+    flux = spectrum.flux
+
+    # Smooth based on the input kernel
+    smoothed_flux = medfilt(flux, width)
+
+    # Return a new object with the smoothed flux.
+    return Spectrum1D(flux=smoothed_flux, spectral_axis=spectrum.spectral_axis,
+                      wcs=spectrum.wcs, unit=spectrum.unit, 
+                      spectral_axis_unit=spectrum.spectral_axis_unit, 
+                      velocity_convention=spectrum.velocity_convention,
+                      rest_value=spectrum.rest_value)
