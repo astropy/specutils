cb4b358d21c70a8bf2a96652d9c9c7cba4397d29
Craig Jones <craig@brechmos.org>
2018-07-20 08:51:49 -0400

    added documentation

--- specutils/utils/spectral_region.py
+++ specutils/utils/spectral_region.py
@@ -2,8 +2,40 @@ import astropy.units as u
 
 
 class SpectralRegion:
+    """
+    SpectralRegion is currently a container class enables some simplicty 
+    in defining and passing a region (interval) for a spectrum.
+
+    In the future, there might be more functionality added in here and there 
+    is some discussion that this might/could move to astropy.region.
+    """
 
     def __init__(self, lower, upper):
+        """
+        Lower and upper values for the interval.
+
+        Parameter
+        --------
+
+        lower: number with ``astropy.units`` unit
+           The lower bound of the region.
+
+        upper: number with ``astropy.units`` unit
+           The upper bound of the region.
+
+        Notes
+        -----
+
+        The ``astropy.units`` for each do not have to be the same, but to retain
+        sanity it might be easiest to have them the same.  The will need to be
+        dispersion units as opposed to flux units.
+        """
+
+        if not isinstance(lower, u.Quantity):
+            raise TypeError('Lower bound of the region must have an astropy.units unit')
+
+        if not isinstance(upper, u.Quantity):
+            raise TypeError('Upper bound of the region must have an astropy.units unit')
 
         self._lower = lower
         self._upper = upper
@@ -14,6 +46,15 @@ class SpectralRegion:
 
     @lower.setter
     def lower(self, value):
+        """
+        Lower bound for the interval.
+
+        Parameter
+        --------
+
+        value: number with ``astropy.units`` unit
+           The lower bound of the region.
+        """
 
         if not isinstance(value, u.Quantity):
             raise TypeError('Lower bound of the region must have an astropy.units unit')
@@ -26,6 +67,16 @@ class SpectralRegion:
 
     @upper.setter
     def upper(self, value):
+        """
+        Upper bound for the interval.
+
+        Parameter
+        --------
+
+        value: number with ``astropy.units`` unit
+           The upper bound of the region.
+        """
+
 
         if not isinstance(value, u.Quantity):
             raise TypeError('Upper bound of the region must have an astropy.units unit')
