ac93cec4bec63d9ee4f93db9866bfb85827e643d
Daniel D'Avella <ddavella@stsci.edu>
2018-08-17 16:00:15 -0400

    Make it clear that full sigma width is being computed

--- specutils/analysis/__init__.py
+++ specutils/analysis/__init__.py
@@ -3,4 +3,4 @@ from __future__ import absolute_import
 from .equivalent_width import *  # noqa
 from .snr import snr  # noqa
 from .centroid import centroid  # noqa
-from .width import sigma # noqa
+from .width import sigma_full_width # noqa
