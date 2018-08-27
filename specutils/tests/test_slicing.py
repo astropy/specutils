cfa34d1264a9ed9701873b3d819b6d34e2131abf
brechmos <craig@brechmos.org>
2018-08-27 07:53:58 -0400

    removed commented out lines

--- specutils/tests/test_slicing.py
+++ specutils/tests/test_slicing.py
@@ -12,7 +12,6 @@ def test_spectral_axes():
     sliced_spec1 = spec1[0]
 
     assert isinstance(sliced_spec1, Spectrum1D)
-    #assert sliced_spec1.wcs == spec1.wcs
     assert_allclose(sliced_spec1.wcs.pixel_to_world(np.arange(10)), spec1.wcs.pixel_to_world(np.arange(10)))
 
     flux2 = np.random.sample((10, 49)) * 100
@@ -23,7 +22,6 @@ def test_spectral_axes():
     sliced_spec2 = spec2[0]
 
     assert isinstance(sliced_spec2, Spectrum1D)
-    #assert sliced_spec2.wcs == spec2.wcs
     assert_allclose(sliced_spec2.wcs.pixel_to_world(np.arange(10)), spec2.wcs.pixel_to_world(np.arange(10)))
     assert sliced_spec2.flux.shape[0] == 49
 
