28dc63668c40dca7537dae6d63f9af4d4e087bba
brechmos <craig@brechmos.org>
2018-09-13 14:53:11 -0400

    added in tests for Ghz

--- specutils/tests/test_region_extract.py
+++ specutils/tests/test_region_extract.py
@@ -38,6 +38,35 @@ def test_region_simple(simulated_spectra):
 
     assert np.allclose(sub_spectrum.flux.value, sub_spectrum_flux_expected)
 
+def test_region_ghz(simulated_spectra):
+    """
+    Test the simple version of the spectral SNR.
+    """
+
+    np.random.seed(42)
+
+    #
+    #  Set up the data and add the uncertainty and calculate the expected SNR
+    #
+
+    spectrum = simulated_spectra.s1_um_mJy_e1
+    spectrum = Spectrum1D(flux=simulated_spectra.s1_um_mJy_e1,
+                          simulated_spectar=simulated_spectra.s1_um_mJy_e1.spectral_axis.frequency)
+
+    region = SpectralRegion(0.6*u.um, 0.8*u.um)
+
+    sub_spectrum = extract_region(spectrum, region)
+
+    sub_spectrum_flux_expected = np.array(
+            [1605.71612173, 1651.41650744, 2057.65798618, 2066.73502361, 1955.75832537,
+             1670.52711471, 1491.10034446, 1637.08084112, 1471.28982259, 1299.19484483,
+             1423.11195734, 1226.74494917, 1572.31888312, 1311.50503403, 1474.05051673,
+             1335.39944397, 1420.61880528, 1433.18623759, 1290.26966668, 1605.67341284,
+             1528.52281708, 1592.74392861, 1568.74162534, 1435.29407808, 1536.68040935,
+             1157.33825995, 1136.12679394,  999.92394692, 1038.61546167, 1011.60297294])
+
+    assert np.allclose(sub_spectrum.flux.value, sub_spectrum_flux_expected)
+
 def test_region_simple_check_ends(simulated_spectra):
     """
     Test the simple version of the spectral SNR.
