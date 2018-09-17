0675fd220f21d4caf015c2436d863d9c28b366ba
Erik Tollerud <erik.tollerud@gmail.com>
2018-09-17 19:48:59 -0400

    change analysis imports to "import *"

--- specutils/analysis/__init__.py
+++ specutils/analysis/__init__.py
@@ -1,6 +1,6 @@
 from __future__ import absolute_import
 
-from .flux import line_flux, equivalent_width  # noqa
-from .snr import snr  # noqa
-from .centroid import centroid  # noqa
-from .width import gaussian_sigma_width, gaussian_fwhm, fwhm # noqa
+from .flux import *  # noqa
+from .snr import *  # noqa
+from .centroid import *  # noqa
+from .width import *  # noqa
