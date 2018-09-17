c498ba05654c00683d84f1e2fb65f5321908f12a
Erik Tollerud <erik.tollerud@gmail.com>
2018-09-17 19:50:45 -0400

    move "centroid" to "location"

--- specutils/analysis/__init__.py
+++ specutils/analysis/__init__.py
@@ -2,5 +2,5 @@ from __future__ import absolute_import
 
 from .flux import *  # noqa
 from .snr import *  # noqa
-from .centroid import *  # noqa
+from .location import *  # noqa
 from .width import *  # noqa
