bbb445049a1edf38b2c42263aa583c8d5ce0d253
Erik Tollerud <erik.tollerud@gmail.com>
2018-09-17 20:16:11 -0400

    change snr module to uncertainty

--- specutils/analysis/__init__.py
+++ specutils/analysis/__init__.py
@@ -1,6 +1,6 @@
 from __future__ import absolute_import
 
 from .flux import *  # noqa
-from .snr import *  # noqa
+from .uncertainty import *  # noqa
 from .location import *  # noqa
 from .width import *  # noqa
