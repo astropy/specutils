d91d7e6f6d954c2834839a746cb4c21be26ea8e1
Erik Tollerud <erik.tollerud@gmail.com>
2018-09-21 11:55:59 -0400

    move over other test

--- specutils/tests/test_unc.py
+++ specutils/tests/test_unc.py
@@ -3,6 +3,7 @@ import numpy as np
 
 import astropy.units as u
 from astropy.tests.helper import quantity_allclose
+from astropy.modeling import models
 
 from ..spectra import Spectrum1D, SpectralRegion
 
@@ -36,3 +37,37 @@ def test_noise_region_unc_multi():
     unc_spec = noise_region_uncertainty(nspec, multi_region)
 
     assert quantity_allclose(unc_spec.uncertainty, 1*u.mJy, atol=.01) #this is a guess at the atol... need to finalize with real data
+
+
+def test_noise_estimate_uncertainty():
+    # an alternate test with a more complicated input spectrum
+
+    np.random.seed(42)
+
+    # Create values for spectrum.
+    frequencies = np.linspace(1, 100, 10000) * u.um
+    g = models.Gaussian1D(amplitude=1*u.Jy, mean=10*u.um, stddev=1*u.um)
+    noise = np.random.normal(0., 0.01, frequencies.shape) * u.Jy
+    flux = g(frequencies) + noise
+
+    # Create the spectrum, spectral region and assign uncertainties
+    spectrum = Spectrum1D(spectral_axis=frequencies, flux=flux)
+    spectral_region = SpectralRegion(50*u.um, 80*u.um)
+    spectrum_with_uncertainty = noise_region_uncertainty(spectrum, spectral_region)
+
+    # Compute the expected.
+    indices = np.nonzero((frequencies >= 50*u.um) & (frequencies <= 80*u.um))
+    expected_uncertainty = np.std(flux[indices])*np.ones(len(frequencies))
+
+    assert quantity_allclose(spectrum_with_uncertainty.uncertainty.array,
+                             expected_uncertainty.value)
+
+    # Same idea, but now with variance.  Create the spectrum, spectral region and assign uncertainties
+    spectrum_with_uncertainty = noise_region_uncertainty(spectrum, spectral_region, np.var)
+
+    # Compute the expected.
+    indices = np.nonzero((frequencies >= 50*u.um) & (frequencies <= 80*u.um))
+    expected_uncertainty = np.var(flux[indices])*np.ones(len(frequencies))
+
+    assert quantity_allclose(spectrum_with_uncertainty.uncertainty.array,
+                             expected_uncertainty.value)
