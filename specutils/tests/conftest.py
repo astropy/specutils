62728e682ad2e5a1232c10d1bddafdc24674c6d3
nmearl <contact@nicholasearl.me>
2018-11-08 15:38:17 -0500

    Finalize remote data retrieval; include gemini, cost, and stis loading

--- specutils/tests/conftest.py
+++ specutils/tests/conftest.py
@@ -4,30 +4,33 @@ import os
 
 import tempfile
 
-
-REMOTE_DATA_IDS = {
-    'cos_fuv': ('1481119', 'COS_FUV.fits')
-}
-
-REMOTE_DATA_NAMES = {
-    'COS_FUV.fits': '1481119'
-}
-
 ACCESS_TOKEN = os.environ.get("ACCESS_TOKEN")
 
 
-@pytest.fixture(scope='module', params=REMOTE_DATA_IDS.values(), ids=REMOTE_DATA_IDS.keys())
-def remote_data(request):
+@pytest.fixture(scope='module')
+def remote_data_path(request):
+    """
+    Remotely access the Zenodo deposition archive to retrieve the versioned
+    test data.
+    """
     if ACCESS_TOKEN is None:
-        raise Exception("Not access token set in the environment.")
+        raise Exception("No access token set in the environment.")
 
+    file_id, file_name = request.param.values()
+
+    # Retrieve information on the deposition files for the given id
     r = requests.get("https://zenodo.org/api/deposit/depositions/{}/files/{}".format(
-        request.param[0], request.param[1]), params={'access_token': ACCESS_TOKEN})
+        file_id, file_name), params={'access_token': ACCESS_TOKEN})
 
+    # Directly download the data file link returned from the deposition info
     f = requests.get(r.json()['links']['download'], params={'access_token': ACCESS_TOKEN})
 
+    # Creating a temporary directory that is automatically cleaned up when the
+    # context is existed, removing any temporarily stored data.
     with tempfile.TemporaryDirectory() as tmp_dir:
-        file_path = os.path.join(tmp_dir, request.param[1])
-        open(file_path, 'wb').write(f.content)
+        file_path = os.path.join(tmp_dir, file_name)
 
-        yield file_path
+        with open(file_path, 'wb') as tmp_file:
+            tmp_file.write(f.content)
+
+            yield file_path
--- specutils/tests/test_loaders.py
+++ specutils/tests/test_loaders.py
@@ -1,25 +1,31 @@
-from __future__ import absolute_import, division
-
 import numpy as np
 from astropy.table import Table
 import astropy.units as u
 import pytest
 from astropy.utils.data import get_pkg_data_filename
 
-from .conftest import remote_data
 from .. import Spectrum1D
+from .conftest import remote_data_path
 
 
-def test_spectrum1d_GMOSfits():
-    optical_fits_file = get_pkg_data_filename('data/L5g_0355+11_Cruz09.fits')
-    optical_spec_2 = Spectrum1D.read(optical_fits_file, format='wcs1d-fits')
+@pytest.mark.parametrize('remote_data_path',
+                         [
+                             {'id': '1481190', 'filename': 'L5g_0355+11_Cruz09.fits'}
+                         ],
+                         indirect=True, scope='function')
+def test_spectrum1d_GMOSfits(remote_data_path):
+    optical_spec_2 = Spectrum1D.read(remote_data_path, format='wcs1d-fits')
 
     assert len(optical_spec_2.data) == 3020
 
 
-def test_specific_spec_axis_unit():
-    optical_fits_file = get_pkg_data_filename('data/L5g_0355+11_Cruz09.fits')
-    optical_spec = Spectrum1D.read(optical_fits_file,
+@pytest.mark.parametrize('remote_data_path',
+                         [
+                             {'id': '1481190', 'filename': 'L5g_0355+11_Cruz09.fits'}
+                         ],
+                         indirect=True, scope='function')
+def test_specific_spec_axis_unit(remote_data_path):
+    optical_spec = Spectrum1D.read(remote_data_path,
                                    spectral_axis_unit="Angstrom",
                                    format='wcs1d-fits')
 
@@ -44,6 +50,28 @@ def test_generic_ecsv_reader(tmpdir):
    assert np.alltrue(spectrum.flux == table['flux'])
    assert np.alltrue(spectrum.uncertainty.array == table['uncertainty'])
 
-def test_auto_load(remote_data):
-    # spec = Spectrum1D.read(remote_data)
-    print(remote_data)
+@pytest.mark.parametrize('remote_data_path',
+                         [
+                             {'id': '1481119', 'filename': 'COS_FUV.fits'},
+                             {'id': '1481181', 'filename': 'COS_NUV.fits'}
+                         ],
+                         indirect=True, scope='function')
+def test_hst_cos(remote_data_path):
+    spec = Spectrum1D.read(remote_data_path, format='HST/COS')
+
+    assert isinstance(spec, Spectrum1D)
+    assert spec.flux.size > 0
+
+
+@pytest.mark.parametrize('remote_data_path',
+                         [
+                             {'id': '1481192', 'filename':'STIS_FUV.fits'},
+                             {'id': '1481185', 'filename': 'STIS_NUV.fits'},
+                             {'id': '1481183', 'filename': 'STIS_CCD.fits'},
+                         ],
+                         indirect=True, scope='function')
+def test_hst_stis(remote_data_path):
+    spec = Spectrum1D.read(remote_data_path, format='HST/STIS')
+
+    assert isinstance(spec, Spectrum1D)
+    assert spec.flux.size > 0
