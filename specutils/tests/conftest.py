618287ad10c7b46f17cee56c03a1a99258099e03
nmearl <contact@nicholasearl.me>
2018-11-09 17:08:26 -0500

    Access data without using the api

--- specutils/tests/conftest.py
+++ specutils/tests/conftest.py
@@ -4,8 +4,6 @@ import os
 
 import tempfile
 
-ZENODO_ACCESS_TOKEN = os.environ.get("ZENODO_ACCESS_TOKEN")
-
 
 @pytest.fixture(scope='module')
 def remote_data_path(request):
@@ -13,17 +11,10 @@ def remote_data_path(request):
     Remotely access the Zenodo deposition archive to retrieve the versioned
     test data.
     """
-    if ZENODO_ACCESS_TOKEN is None:
-        raise Exception("No access token set in the environment.")
-
     file_id, file_name = request.param.values()
 
-    # Retrieve information on the deposition files for the given id
-    r = requests.get("https://zenodo.org/api/deposit/depositions/{}/files/{}".format(
-        file_id, file_name), params={'access_token': ZENODO_ACCESS_TOKEN})
-
-    # Directly download the data file link returned from the deposition info
-    f = requests.get(r.json()['links']['download'], params={'access_token': ZENODO_ACCESS_TOKEN})
+    f = requests.get("https://zenodo.org/record/{}/files/{}?download=1".format(
+        file_id, file_name))
 
     # Create a temporary directory that is automatically cleaned up when the
     # context is exited, removing any temporarily stored data.
