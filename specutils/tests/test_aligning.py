734b1de33deab0650ff822e7bc7a79a345bec2c7
Craig Jones <craig@brechmos.org>
2018-12-14 12:39:49 -0500

    added aligning test

---
+++ specutils/tests/test_aligning.py
@@ -0,0 +1,31 @@
+import os
+import astropy.units as u
+import numpy as np
+import pytest
+
+from astropy.io import fits, misc
+from ..spectra.spectrum1d import Spectrum1D
+from ..manipulation import align_spectra
+from .spectral_examples import simulated_spectra
+
+
+def test_aligning():
+
+    data = misc.fnunpickle('data/mos-nrs1.pck')
+    
+    wave1 = data['spectrum1']['wavelengths']
+    spec1 = data['spectrum1']['flux']
+
+    wave2 = data['spectrum2']['wavelengths']
+    spec2 = data['spectrum2']['flux']
+
+    spectrum1 = Spectrum1D(flux=spec1, spectral_axis=wave1)
+    spectrum2 = Spectrum1D(flux=spec2, spectral_axis=wave2)
+
+    out_spectrum = align_spectra(spectrum1, spectrum2)
+
+    assert np.allclose(out_spectrum.spectral_axis.value, data['out_spectrum']['wavelengths'].value)
+    assert out_spectrum.spectral_axis.unit == data['out_spectrum']['wavelengths'].unit
+
+    assert np.allclose(out_spectrum.flux.value, data['out_spectrum']['flux'].value)
+    assert out_spectrum.flux.unit == data['out_spectrum']['flux'].unit
