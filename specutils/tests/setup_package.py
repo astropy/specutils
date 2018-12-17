47d39aec5e44deb21445c85d2874b68cffb41a55
Craig Jones <craig@brechmos.org>
2018-12-17 09:39:23 -0500

    included pck file for tests

--- specutils/tests/setup_package.py
+++ specutils/tests/setup_package.py
@@ -3,5 +3,6 @@ import os
 def get_package_data():
     paths = ['coveragerc',
              os.path.join('data', '*fits')]
+             os.path.join('data', '*pck')]
 
     return {'specutils.tests': paths}
