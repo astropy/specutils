eac00089e0644b66727410ddd0ba99710daf61ce
Craig Jones <craig@brechmos.org>
2018-12-17 09:55:19 -0500

    fixed error

--- specutils/tests/setup_package.py
+++ specutils/tests/setup_package.py
@@ -2,7 +2,7 @@ import os
 
 def get_package_data():
     paths = ['coveragerc',
-             os.path.join('data', '*fits')]
+             os.path.join('data', '*fits'),
              os.path.join('data', '*pck')]
 
     return {'specutils.tests': paths}
