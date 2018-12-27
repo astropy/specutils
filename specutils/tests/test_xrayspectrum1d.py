c760cf4b3f296eddc9c67b5ca1347f22e34f5730
Lia Corrales <liac@umich.edu>
2018-12-27 16:55:08 -0700

    added basic tests for a fake arf

--- specutils/tests/test_xrayspectrum1d.py
+++ specutils/tests/test_xrayspectrum1d.py
@@ -1,7 +1,7 @@
 import numpy as np
 import astropy.units as u
 from astropy.modeling.powerlaws import PowerLaw1D
-from specutils import XraySpectrum1D
+from specutils import XraySpectrum1D, AreaResponse, ResponseMatrix
 
 def test_create_from_arrays():
     # Test that XraySpectrum1D can be initialized
@@ -27,8 +27,29 @@ def test_call_spectrum():
 def test_apply_model():
     # Test that one can evaluate XraySpectrum1D with a model
     test_spec = test_create_from_arrays()
-    new_model = PowerLaw1D(amplitude=5.e-3, alpha=1.8, x_0=1.e3)
+    new_model = PowerLaw1D(amplitude=5.e-3, alpha=1.8, x_0=1.0)
     model_flux = new_model(test_spec.spectral_axis.value)
     ymodel = test_spec.apply_response(model_flux)
     assert len(ymodel) == len(test_spec.spectral_axis)
+    # Since there's no arf or rmf, should get the same array out
+    assert np.all(ymodel == model_flux)
+
+    # Now add an arf
+    specresp = np.ones_like(test_spec.bin_lo.value) * 2.0
+    arf = AreaResponse(test_spec.bin_lo, test_spec.bin_hi,
+                       specresp, test_spec.exposure)
+    test_spec.assign_arf(arf)
+
+    # Test that the arf is applied properly
+    ymodel2 = test_spec.apply_response(model_flux)
+    # The arf also gets multiplied by the exposure time, so that needs
+    # to be included in this check
+    check_value = ymodel2 / (model_flux * test_spec.exposure)
+    assert np.all(check_value == 2.0)
+    return
+
+## Currently there is no RMF check because it requires a test data file
+## However, we can test that it can be initialized with empty values
+def test_init_rmf():
+    rmf = ResponseMatrix()
     return
