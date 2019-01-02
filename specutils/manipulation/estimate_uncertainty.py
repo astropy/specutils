dcb16677dfaacb6e491a15ca281b2eeee4b07a99
Craig Jones <craig@brechmos.org>
2019-01-02 10:50:00 -0500

    add in ck for unknown units for uncertainty calculation

--- specutils/manipulation/estimate_uncertainty.py
+++ specutils/manipulation/estimate_uncertainty.py
@@ -38,7 +38,6 @@ def noise_region_uncertainty(spectrum, spectral_region, noise_func=np.std):
     # Extract the sub spectrum based on the region
     sub_spectra = extract_region(spectrum, spectral_region)
 
-
     # TODO: make this work right for multi-dimensional spectrum1D's?
     if not isinstance(sub_spectra, list):
         sub_spectra = [sub_spectra]
@@ -56,6 +55,8 @@ def noise_region_uncertainty(spectrum, spectral_region, noise_func=np.std):
         uncertainty = VarianceUncertainty(noise*np.ones(spectrum.flux.shape))
     elif noise.unit == 1/(spectrum.flux.unit**2):
         uncertainty = InverseVariance(noise*np.ones(spectrum.flux.shape))
+    else:
+        raise ValueError('Can not determine correct NDData Uncertainty based on units {} relative to the flux units {}'.format(noise.unit, spectrum.flux.unit))
 
     # Return new specturm with uncertainty set.
     return Spectrum1D(flux=spectrum.flux, spectral_axis=spectrum.spectral_axis,
