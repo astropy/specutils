5423f2b8c23eb735022cbc12a65888be79186cf0
Daniel D'Avella <drdavella@gmail.com>
2019-02-05 13:07:10 -0500

    Add ASDF tag implementation for Spectrum1D

--- specutils/io/asdf/tags/spectra.py
+++ specutils/io/asdf/tags/spectra.py
@@ -1,3 +1,7 @@
+from astropy.tests.helper import quantity_allclose
+
+from asdf.yamlutil import custom_tree_to_tagged_tree, tagged_tree_to_custom_tree
+
 from ..types import SpecutilsType
 from ....spectra import Spectrum1D, SpectrumList
 
@@ -6,8 +10,42 @@ __all__ = ['Spectrum1DType', 'SpectrumListType']
 
 
 class Spectrum1DType(SpecutilsType):
-    pass
+    name = 'spectra/spectrum1d'
+    types = [Spectrum1D]
+    version = '1.0.0'
+
+    @classmethod
+    def to_tree(cls, obj, ctx):
+
+        node = {}
+        node['flux'] = custom_tree_to_tagged_tree(obj.flux, ctx)
+        node['spectral_axis'] = custom_tree_to_tagged_tree(obj.spectral_axis, ctx)
+
+        return node
+
+    @classmethod
+    def from_tree(cls, tree, ctx):
+
+        flux = tagged_tree_to_custom_tree(tree['flux'], ctx)
+        spectral_axis = tagged_tree_to_custom_tree(tree['spectral_axis'], ctx)
+
+        return Spectrum1D(flux=flux, spectral_axis=spectral_axis)
+
+    @classmethod
+    def assert_equal(cls, old, new):
+        assert quantity_allclose(old.flux, new.flux)
+        assert quantity_allclose(old.spectral_axis, new.spectral_axis)
 
 
 class SpectrumListType(SpecutilsType):
-    pass
+    name = 'spectra/spectrum_list'
+    types = [SpectrumList]
+    version = '1.0.0'
+
+    @classmethod
+    def to_tree(cls, obj, ctx):
+        pass
+
+    @classmethod
+    def from_tree(cls, tree, ctx):
+        pass
