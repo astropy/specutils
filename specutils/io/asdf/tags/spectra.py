4858aa1413082b92f4528d3c9daee0fef9199506
Daniel D'Avella <drdavella@gmail.com>
2019-02-05 18:30:47 -0500

    Add uncertainty to ASDF serialization of Spectrum1D

--- specutils/io/asdf/tags/spectra.py
+++ specutils/io/asdf/tags/spectra.py
@@ -1,5 +1,4 @@
-from astropy.tests.helper import quantity_allclose
-
+import astropy.nddata
 from asdf.yamlutil import custom_tree_to_tagged_tree, tagged_tree_to_custom_tree
 
 from ..types import SpecutilsType
@@ -7,6 +6,14 @@ from ....spectra import Spectrum1D, SpectrumList
 
 
 __all__ = ['Spectrum1DType', 'SpectrumListType']
+
+
+UNCERTAINTY_TYPE_MAPPING = {
+    'std': astropy.nddata.StdDevUncertainty,
+    'var': astropy.nddata.VarianceUncertainty,
+    'ivar': astropy.nddata.InverseVariance,
+    'unknown': astropy.nddata.UnknownUncertainty,
+}
 
 
 class Spectrum1DType(SpecutilsType):
@@ -20,6 +27,11 @@ class Spectrum1DType(SpecutilsType):
         node = {}
         node['flux'] = custom_tree_to_tagged_tree(obj.flux, ctx)
         node['spectral_axis'] = custom_tree_to_tagged_tree(obj.spectral_axis, ctx)
+        if obj.uncertainty is not None:
+            node['uncertainty'] = {}
+            node['uncertainty']['uncertainty_type'] = obj.uncertainty.uncertainty_type
+            data = custom_tree_to_tagged_tree(obj.uncertainty.array, ctx)
+            node['uncertainty']['data'] = data
 
         return node
 
@@ -28,13 +40,28 @@ class Spectrum1DType(SpecutilsType):
 
         flux = tagged_tree_to_custom_tree(tree['flux'], ctx)
         spectral_axis = tagged_tree_to_custom_tree(tree['spectral_axis'], ctx)
+        uncertainty = tree.get('uncertainty', None)
+        if uncertainty is not None:
+            klass = UNCERTAINTY_TYPE_MAPPING[uncertainty['uncertainty_type']]
+            data = tagged_tree_to_custom_tree(uncertainty['data'], ctx)
+            uncertainty = klass(data)
 
-        return Spectrum1D(flux=flux, spectral_axis=spectral_axis)
+        return Spectrum1D(flux=flux, spectral_axis=spectral_axis,
+                          uncertainty=uncertainty)
 
     @classmethod
     def assert_equal(cls, old, new):
+        from numpy.testing import assert_allclose
+        from astropy.tests.helper import quantity_allclose
+
         assert quantity_allclose(old.flux, new.flux)
         assert quantity_allclose(old.spectral_axis, new.spectral_axis)
+        if old.uncertainty is None:
+            assert new.uncertainty is None
+        else:
+            assert old.uncertainty.uncertainty_type == new.uncertainty.uncertainty_type
+            assert_allclose(old.uncertainty.array, new.uncertainty.array)
+
 
 
 class SpectrumListType(SpecutilsType):
--- specutils/io/asdf/tags/tests/test_spectra.py
+++ specutils/io/asdf/tags/tests/test_spectra.py
@@ -7,15 +7,25 @@ from asdf.tests.helpers import assert_roundtrip_tree
 from specutils import Spectrum1D, SpectrumList
 
 
-def create_spectrum1d(xmin, xmax):
+def create_spectrum1d(xmin, xmax, uncertainty=None):
     flux = np.ones(xmax-xmin) * u.Jy
     wavelength = np.arange(xmin, xmax) * u.AA
-    return Spectrum1D(spectral_axis=wavelength, flux=flux)
+    uncertainty = np.ones(xmax-xmin) if uncertainty is not None else None
+    return Spectrum1D(spectral_axis=wavelength, flux=flux,
+                      uncertainty=uncertainty)
 
 
 def test_asdf_spectrum1d(tmpdir):
 
     spectrum = create_spectrum1d(5100, 5300)
+
+    tree = dict(spectrum=spectrum)
+    assert_roundtrip_tree(tree, tmpdir)
+
+
+def test_asdf_spectrum1d_uncertainty(tmpdir):
+
+    spectrum = create_spectrum1d(5100, 5300, uncertainty=True)
 
     tree = dict(spectrum=spectrum)
     assert_roundtrip_tree(tree, tmpdir)
