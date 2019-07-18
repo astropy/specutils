7805915f164fa59a2378fd9a94b14739475a854e
Jesse Averbukh <javerbukh@stsci.edu>
2019-07-18 10:11:32 -0400

    improved variable names and documentation

--- specutils/analysis/template_match.py
+++ specutils/analysis/template_match.py
@@ -4,33 +4,41 @@ from ..manipulation import FluxConservingResample
 from scipy.stats import chisquare
 
 def _template_match(observed_spectrum, template_spectrum):
-    # resample the template spectrum to match the wavelength of the observed spectrum
+    """
+    Resample the template spectrum to match the wavelength of the observed spectrum.
+    Then, run scipy.stats.chisquare on the flux of the two spectra.
+
+    :param observed_spectrum: The observed spectrum
+    :param template_spectrum: The template spectrum, which will be resampled to match the wavelength of the observed
+        spectrum
+    :return: chi square of the flux of the observed spectrum and the flux of the template spectrum
+    """
     fluxc_resample = FluxConservingResample()
-    output_spectrum1D = fluxc_resample(template_spectrum, observed_spectrum.wavelength)
+    template_spectrum1D = fluxc_resample(template_spectrum, observed_spectrum.wavelength)
 
     # calculate chi square
-    x2 = chisquare(observed_spectrum.flux, output_spectrum1D.flux)
+    x2 = chisquare(observed_spectrum.flux, template_spectrum1D.flux)
     return x2
 
-def template_match(observed_spectrum, collection):
+def template_match(observed_spectrum, spectral_templates):
     """
     Find what instance collection is and run _template_match accordingly
     """
-    if isinstance(collection, Spectrum1D):
-        return Spectrum1D, _template_match(observed_spectrum, collection)
+    if isinstance(spectral_templates, Spectrum1D):
+        return Spectrum1D, _template_match(observed_spectrum, spectral_templates)
 
-    elif isinstance(collection, SpectrumCollection):
+    elif isinstance(spectral_templates, SpectrumCollection):
         pass
 
     # Loop through spectra in list and return spectrum with lowest chi square
     # and its corresponding chi square
-    elif isinstance(collection, list):
-        min = None
+    elif isinstance(spectral_templates, list):
+        chi2_min = None
         smallest_chi_spec = None
-        for spectrum in collection:
-            chi = _template_match(observed_spectrum, spectrum)
-            if min is None or chi < min:
-                min = chi
+        for spectrum in spectral_templates:
+            chi2 = _template_match(observed_spectrum, spectrum)
+            if chi2_min is None or chi2 < chi2_min:
+                chi2_min = chi2
                 smallest_chi_spec = spectrum
 
-        return smallest_chi_spec, min
+        return smallest_chi_spec, chi2_min
