Fix lag computation.

specutils/analysis/correlation.py
@@ -22,9 +22,9 @@ def _normalize(observed_spectrum, template_spectrum):
         A float which will normalize the template spectrum's flux so that it
         can be compared to the observed spectrum.
     """
-    num = np.sum((observed_spectrum.flux*template_spectrum.flux)/
+    num = np.nansum((observed_spectrum.flux*template_spectrum.flux)/
                  (observed_spectrum.uncertainty.array**2))
-    denom = np.sum((template_spectrum.flux/
+    denom = np.nansum((template_spectrum.flux/
                     observed_spectrum.uncertainty.array)**2)
 
     return num/denom
@@ -59,6 +59,8 @@ def template_correlate(observed_spectrum, template_spectrum):
     equiv = getattr(u.equivalencies, 'doppler_{0}'.format(
         observed_spectrum.velocity_convention))(observed_spectrum.rest_value)
 
-    lag = observed_spectrum.spectral_axis.to(u.km / u.s, equivalencies=equiv)
+    lags = observed_spectrum.spectral_axis.to(u.km / u.s, equivalencies=equiv)
+    diff = lags[1:] - lags[:-1]
+    lags = (np.array(range(len(corr))) - len(corr)/2 + 0.5) * diff[0]
 
-    return (corr * u.dimensionless_unscaled, lag)
+    return (corr * u.dimensionless_unscaled, lags)

specutils/tests/test_correlation.py
@@ -1,3 +1,4 @@
+import os
 import numpy as np
 import astropy.units as u
 
@@ -7,7 +8,7 @@ from astropy.modeling import models
 from ..spectra.spectrum1d import Spectrum1D
 from ..analysis import correlation
 
-SIZE = 40
+SIZE = 41 # force symmetry around peak.
 
 
 def test_correlation():
@@ -24,8 +25,9 @@ def test_correlation():
     # as to generate a correlation peak at a expected lag.
     f1 = np.random.randn(SIZE) * u.Jy
     f2 = np.random.randn(SIZE) * u.Jy
-    g1 = models.Gaussian1D(amplitude=30 * u.Jy, mean=5020 * u.AA, stddev=2 * u.AA)
-    g2 = models.Gaussian1D(amplitude=30 * u.Jy, mean=5023 * u.AA, stddev=2 * u.AA)
+
+    g1 = models.Gaussian1D(amplitude=30 * u.Jy, mean=5025 * u.AA, stddev=2 * u.AA)
+    g2 = models.Gaussian1D(amplitude=30 * u.Jy, mean=5020 * u.AA, stddev=2 * u.AA)
 
     flux1 = f1 + g1(spec_axis)
     flux2 = f2 + g2(spec_axis)
@@ -48,12 +50,11 @@ def test_correlation():
     assert corr.unit == u.dimensionless_unscaled
     assert lag.unit == u.km / u.s
 
-    # Check that lag at mid-point is zero and lags are symmetrical
-    midpoint = int(len(lag) / 2)
-    assert int((lag[midpoint]).value) == 0
-    np.testing.assert_almost_equal(lag[midpoint+10].value, (-(lag[midpoint-10])).value, 0.01)
-
     # Check position of correlation peak.
     maximum = np.argmax(corr)
-    assert maximum == 36
-    np.testing.assert_almost_equal(lag[maximum].value, 980., 0.1)
+    assert maximum == 45
+    np.testing.assert_almost_equal(lag[maximum].value, 298.6, 1)
+
+    # Check that lags are symmetrical
+    midpoint = int(len(lag) / 2)
+    np.testing.assert_almost_equal(lag[midpoint+2].value, (-(lag[midpoint-2])).value, 2)