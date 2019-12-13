08cd8ff1ee387db2fb9a6eb298b124572fe1d4c3
nmearl <contact@nicholasearl.me>
2019-12-13 11:34:40 -0700

    Update package to remove astropy_helpers

--- specutils/_astropy_init.py
+++ specutils/_astropy_init.py
@@ -17,10 +17,6 @@ try:
     from .version import version as __version__
 except ImportError:
     __version__ = ''
-try:
-    from .version import githash as __githash__
-except ImportError:
-    __githash__ = ''
 
 
 if not _ASTROPY_SETUP_:  # noqa
--- specutils/tests/setup_package.py
+++
@@ -1,7 +0,0 @@
-import os
-
-def get_package_data():
-    paths = ['coveragerc',
-             os.path.join('data', '*fits')]
-
-    return {'specutils.tests': paths}
