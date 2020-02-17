3297b8c3f91f3ede03941ab18753b240b3a0cf5f
busko <busko@stsci.edu>
2020-02-17 15:54:02 -0500

    Add autocorrelation test.

--- specutils/tests/test_correlation.py
+++ specutils/tests/test_correlation.py
@@ -10,27 +10,71 @@ from ..spectra.spectrum1d import Spectrum1D
 from ..analysis import correlation
 from ..manipulation.resample import FluxConservingResampler
 
-SIZE = 1001
 
 DATA_DIR = os.path.join(os.path.dirname(__file__), '../../')
+
+
+def test_autocorrelation():
+    """
+    Test auto correlation
+    """
+    size = 41
+
+    # Seed np.random so that results are consistent
+    np.random.seed(41)
+
+    # Create test spectra
+    spec_axis = np.linspace(5000., 5040., num=size) * u.AA
+    f1 = np.random.randn(size) * u.Jy
+    g1 = models.Gaussian1D(amplitude=30 * u.Jy, mean=5020 * u.AA, stddev=2 * u.AA)
+    flux1 = f1 + g1(spec_axis)
+
+    # Observed spectrum must have a rest wavelength value set in.
+    spec1 = Spectrum1D(spectral_axis=spec_axis,
+                      flux=flux1,
+                      uncertainty=StdDevUncertainty(np.random.sample(size), unit='Jy'),
+                      velocity_convention='optical',
+                      rest_value=5020.*u.AA)
+
+    spec2 = Spectrum1D(spectral_axis=spec_axis,
+                       flux=flux1,
+                       uncertainty=StdDevUncertainty(np.random.sample(size), unit='Jy'))
+
+    # Get result from correlation
+    corr, lag = correlation.template_correlate(spec1, spec2)
+
+    # Check units
+    assert corr.unit == u.dimensionless_unscaled
+    assert lag.unit == u.km / u.s
+
+    # Check that lags are symmetrical
+    midpoint = int(len(lag) / 2)
+    np.testing.assert_almost_equal(lag[midpoint+11].value, (-(lag[midpoint-11])).value, 2)
+
+    # Check position of correlation peak.
+    maximum = np.argmax(corr)
+    assert maximum == size-1
+    np.testing.assert_almost_equal(lag[maximum].value, 0.0, 1)
 
 
 def test_correlation():
     """
     Test correlation when both observed and template spectra have the same wavelength axis
     """
+    size = 1001
+
     # Seed np.random so that results are consistent
-    np.random.seed(41)
+    np.random.seed(51)
 
     # Create test spectra
-    spec_axis = np.linspace(4500., 6500., num=SIZE) * u.AA
+    spec_axis = np.linspace(4500., 6500., num=size) * u.AA
 
     # Two narrow Gaussians are offset from each other so
     # as to generate a correlation peak at a expected lag.
-    # f1 = np.random.randn(SIZE)*0.0001 * u.Jy
-    # f2 = np.random.randn(SIZE)*0.0001 * u.Jy
-    f1 = (np.ones(SIZE)-0.5) * u.Jy
-    f2 = (np.ones(SIZE)-0.5) * u.Jy
+    f1 = np.random.randn(size)*0.5 * u.Jy
+    f2 = np.random.randn(size)*0.5 * u.Jy
+    # f1 = (np.ones(size)-0.5) * u.Jy
+    # f2 = (np.ones(size)-0.5) * u.Jy
 
     rest_value = 6000. * u.AA
 
@@ -47,13 +91,13 @@ def test_correlation():
     # Observed spectrum must have a rest wavelength value set in.
     spec1 = Spectrum1D(spectral_axis=spec_axis,
                       flux=flux1,
-                      uncertainty=StdDevUncertainty(np.random.sample(SIZE), unit='Jy'),
+                      uncertainty=StdDevUncertainty(np.random.sample(size), unit='Jy'),
                       velocity_convention='optical',
                       rest_value=rest_value)
 
     spec2 = Spectrum1D(spectral_axis=spec_axis,
                        flux=flux2,
-                       uncertainty=StdDevUncertainty(np.random.sample(SIZE), unit='Jy'))
+                       uncertainty=StdDevUncertainty(np.random.sample(size), unit='Jy'))
 
     # Get result from correlation
     corr, lag = correlation.template_correlate(spec1, spec2)
