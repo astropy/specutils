d01c0d6bb2068467d9af18d455f137878340380b
nmearl <contact@nicholasearl.me>
2020-02-17 08:55:22 -0500

    Update template to be consistent with astropy

--- setup.py
+++ setup.py
@@ -1,7 +1,17 @@
 #!/usr/bin/env python
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
 
+# NOTE: The configuration for the package, including the name, version, and
+# other information are set in the setup.cfg file.
+
+import os
 import sys
+
 from setuptools import setup
+
+
+# First provide helpful messages if contributors try and run legacy commands
+# for tests or docs.
 
 TEST_HELP = """
 Note: running tests is no longer done using 'python setup.py test'. Instead
@@ -16,7 +26,7 @@ If you don't already have tox installed, you can install it with:
 If you only want to run part of the test suite, you can also use pytest
 directly with::
 
-    pip install -e .
+    pip install -e .[test]
     pytest
 
 For more information, see:
@@ -38,6 +48,12 @@ If you don't already have tox installed, you can install it with:
 
     pip install tox
 
+You can also build the documentation with Sphinx directly using::
+
+    pip install -e .[docs]
+    cd docs
+    make html
+
 For more information, see:
 
   http://docs.astropy.org/en/latest/install.html#builddocs
@@ -47,4 +63,16 @@ if 'build_docs' in sys.argv or 'build_sphinx' in sys.argv:
     print(DOCS_HELP)
     sys.exit(1)
 
-setup(use_scm_version={'write_to': os.path.join('specutils', 'version.py')})
+VERSION_TEMPLATE = """
+# Note that we need to fall back to the hard-coded version if either
+# setuptools_scm can't be imported or setuptools_scm can't determine the
+# version, so we catch the generic 'Exception'.
+try:
+    from setuptools_scm import get_version
+    version = get_version(root='..', relative_to=__file__)
+except Exception:
+    version = '{version}'
+""".lstrip()
+
+setup(use_scm_version={'write_to': os.path.join('specutils', 'version.py'),
+                       'write_to_template': VERSION_TEMPLATE})
