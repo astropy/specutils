Fix sign of analysis function outputs

specutils/analysis/flux.py
@@ -97,7 +97,7 @@ def _compute_line_flux(spectrum, regions=None):
         calc_spectrum = spectrum
 
     # Average dispersion in the line region
-    avg_dx = np.diff(calc_spectrum.spectral_axis.quantity)
+    avg_dx = np.abs(np.diff(calc_spectrum.spectral_axis.quantity))
 
     line_flux = np.sum(calc_spectrum.flux[1:] * avg_dx)
 
@@ -116,7 +116,9 @@ def _compute_equivalent_width(spectrum, continuum=1, regions=None):
         continuum = 1*calc_spectrum.flux.unit
 
     spectral_axis = calc_spectrum.spectral_axis.quantity
-    dx = spectral_axis[-1] - spectral_axis[0]
+
+    # Ensure that the
+    dx = np.abs(spectral_axis[-1] - spectral_axis[0])
 
     line_flux = _compute_line_flux(spectrum, regions)
 

specutils/analysis/width.py
@@ -258,7 +258,7 @@ def _compute_single_fwhm(flux, spectral_axis):
                        / (flux_value[i1] - left_flux)
                        + left_spectral)
 
-    return spectral_axis.unit * (right_value - left_value)
+    return spectral_axis.unit * np.abs(right_value - left_value)
 
 
 def _compute_fwhm(spectrum, regions=None):

specutils/tests/test_analysis.py
@@ -33,7 +33,7 @@ def test_line_flux():
 
     # Account for the fact that Astropy uses a different normalization of the
     # Gaussian where the integral is not 1
-    expected = -np.sqrt(2*np.pi) * u.GHz * u.Jy
+    expected = np.sqrt(2*np.pi) * u.GHz * u.Jy
 
     assert quantity_allclose(result, expected, atol=0.01*u.GHz*u.Jy)
 
@@ -55,7 +55,7 @@ def test_equivalent_width():
 
     # Since this is an emission line, we expect the equivalent width value to
     # be negative
-    expected = (np.sqrt(2*np.pi) * u.GHz)
+    expected = -(np.sqrt(2*np.pi) * u.GHz)
 
     assert quantity_allclose(result, expected, atol=0.01*u.GHz)
 
@@ -73,7 +73,7 @@ def test_equivalent_width_regions():
     cont_norm_spec = spec / np.median(spec.flux)
     result = equivalent_width(cont_norm_spec, regions=SpectralRegion(97*u.GHz, 3*u.GHz))
 
-    expected = (np.sqrt(2*np.pi) * u.GHz)
+    expected = -(np.sqrt(2*np.pi) * u.GHz)
 
     assert quantity_allclose(result, expected, atol=0.02*u.GHz)
 
@@ -94,7 +94,9 @@ def test_equivalent_width_continuum(continuum):
 
     assert result.unit.is_equivalent(spectrum.wcs.unit)
 
-    expected = (np.sqrt(2*np.pi) * u.GHz) / continuum.value
+    # Since this is an emission line, we expect the equivalent width value to
+    # be negative
+    expected = -(np.sqrt(2*np.pi) * u.GHz) / continuum.value
 
     assert quantity_allclose(result, expected, atol=0.01*u.GHz)
 
@@ -116,7 +118,7 @@ def test_equivalent_width_absorption():
 
     assert result.unit.is_equivalent(spectrum.wcs.unit)
 
-    expected = -amplitude*np.sqrt(2*np.pi) * u.GHz
+    expected = amplitude*np.sqrt(2*np.pi) * u.GHz
 
     assert quantity_allclose(result, expected, atol=0.01*u.GHz)
 