291568b875ddde71ce002db0618bbb4db52f6780
Derek Homeier <dhomeie@gwdg.de>
2020-04-25 15:36:01 +0200

    ENH: improve SDSS unit detection; extend identification to file/hdulist objects

--- specutils/tests/test_loaders.py
+++ specutils/tests/test_loaders.py
@@ -140,10 +140,10 @@ def test_sdss_spec():
         assert spec.flux.size > 0
 
     with urllib.request.urlopen('https://dr14.sdss.org/optical/spectrum/view/data/format%3Dfits/spec%3Dlite?mjd=55359&fiberid=596&plateid=4055') as response:
-        with tempfile.NamedTemporaryFile(prefix=sp_pattern) as tmp_file:
+        with tempfile.NamedTemporaryFile() as tmp_file:
             shutil.copyfileobj(response, tmp_file)
 
-            # Read from local disk via filename
+            # Read from local disk via file signature
             spec = Spectrum1D.read(tmp_file.name)
 
             assert isinstance(spec, Spectrum1D)
@@ -151,7 +151,13 @@ def test_sdss_spec():
 
             # Read from HDUList object
             hdulist = fits.open(tmp_file.name)
-            spec = Spectrum1D.read(hdulist, format="SDSS-III/IV spec")
+            spec = Spectrum1D.read(hdulist)
+            assert isinstance(spec, Spectrum1D)
+            assert spec.flux.size > 0
+
+            # Read from file handle
+            fileio = open(tmp_file.name, mode='rb')
+            spec = Spectrum1D.read(fileio)
             assert isinstance(spec, Spectrum1D)
             assert spec.flux.size > 0
 
@@ -166,23 +172,28 @@ def test_sdss_spspec():
         assert spec.flux.size > 0
 
     with urllib.request.urlopen('http://das.sdss.org/spectro/1d_26/0273/1d/spSpec-51957-0273-016.fit') as response:
-        with tempfile.NamedTemporaryFile(prefix=sp_pattern) as tmp_file:
+        with tempfile.NamedTemporaryFile() as tmp_file:
             shutil.copyfileobj(response, tmp_file)
 
-            # Read from local disk via filename
+            # Read from local disk via file signature
             with warnings.catch_warnings():
                 warnings.simplefilter('ignore', FITSFixedWarning)
-                spec = Spectrum1D.read(tmp_file.name, format="SDSS-I/II spSpec")
+                spec = Spectrum1D.read(tmp_file.name)
 
             assert isinstance(spec, Spectrum1D)
             assert spec.flux.size > 0
 
             # Read from HDUList object
             hdulist = fits.open(tmp_file.name)
-            spec = Spectrum1D.read(hdulist, format="SDSS-I/II spSpec")
+            spec = Spectrum1D.read(hdulist)
             assert isinstance(spec, Spectrum1D)
             assert spec.flux.size > 0
 
+            # Read from file handle
+            fileio = open(tmp_file.name, mode='rb')
+            spec = Spectrum1D.read(fileio)
+            assert isinstance(spec, Spectrum1D)
+            assert spec.flux.size > 0
 
 @pytest.mark.remote_data
 def test_sdss_spec_stream():
