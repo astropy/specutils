Allow pixel units in SpectralAxis and fix tests

specutils/analysis/width.py
@@ -207,7 +207,7 @@ def _compute_gaussian_sigma_width(spectrum, regions=None):
         spectral_axis = np.broadcast_to(spectral_axis, flux.shape, subok=True)
         centroid_result = centroid_result[:, np.newaxis]
 
-    dx = (spectral_axis - centroid_result).quantity
+    dx = (spectral_axis - centroid_result)
     sigma = np.sqrt(np.sum((dx * dx) * flux, axis=-1) / np.sum(flux, axis=-1))
 
     return sigma

specutils/spectra/spectrum1d.py
@@ -3,11 +3,9 @@ from copy import deepcopy
 
 import numpy as np
 from astropy import units as u
-from astropy import constants as cnst
 from astropy.nddata import NDDataRef
 from astropy.utils.decorators import lazyproperty
 from .spectrum_mixin import OneDSpectrumMixin
-from ..extern.spectralcoord import SpectralCoord
 from .spectral_axis import SpectralAxis
 from ..utils.wcs_utils import gwcs_from_array
 
@@ -185,6 +183,9 @@ class Spectrum1D(OneDSpectrumMixin, NDDataRef):
             # If spectral_axis wasn't provided, set _spectral_axis based on
             # the WCS
             spec_axis = self.wcs.pixel_to_world(np.arange(self.flux.shape[-1]))
+
+            if spec_axis.unit.is_equivalent(u.one):
+                spec_axis = spec_axis * u.pixel
 
             self._spectral_axis = SpectralAxis(
                 spec_axis,

specutils/tests/test_spectrum1d.py
@@ -149,8 +149,8 @@ def test_redshift():
                       rest_value=6000 * u.AA,
                       redshift= 0.1)
 
-    assert u.allclose(spec.velocity, [-69951.3, 29979.2, 129910.1]*u.km/u.s,
-                     atol=0.5*u.km/u.s)
+    assert u.allclose(spec.velocity, [-71443.75318854, 28487.0661448, 128417.88547813]*u.km/u.s,
+                      atol=0.5*u.km/u.s)
 
     #-------------------------
 
@@ -160,7 +160,7 @@ def test_redshift():
                       rest_value=11.0 * u.GHz)
 
     assert u.allclose(spec.velocity, [13626., 0, -13626]*u.km/u.s,
-                     atol=1*u.km/u.s)
+                      atol=1*u.km/u.s)
 
     spec = Spectrum1D(flux=np.array([26.0, 30.0, 44.5]) * u.Jy,
                       spectral_axis=np.array([10.5, 11.0, 11.5]) * u.GHz,
@@ -168,7 +168,7 @@ def test_redshift():
                       rest_value=11.0 * u.GHz,
                       redshift= 0.1)
 
-    assert u.allclose(spec.velocity, [43606., 29979., 16352.]*u.km/u.s,
+    assert u.allclose(spec.velocity, [42113.99605389, 28487.0661448 , 14860.13623571]*u.km/u.s,
                       atol=1*u.km/u.s)
 
     #------------------------- radial velocity mode
@@ -329,7 +329,7 @@ def test_repr():
                                flux=np.random.random(10) * u.Jy)
     result = repr(spec_with_wcs)
     assert result.startswith('<Spectrum1D(flux=<Quantity [')
-    assert 'spectral_axis=<SpectralAxis [' in result
+    assert 'spectral_axis=<SpectralAxis' in result
 
     spec_with_unc = Spectrum1D(spectral_axis=np.linspace(100, 1000, 10) * u.nm,
                                flux=np.random.random(10) * u.Jy,
@@ -337,7 +337,7 @@ def test_repr():
                                    np.random.sample(10), unit='Jy'))
     result = repr(spec_with_unc)
     assert result.startswith('<Spectrum1D(flux=<Quantity [')
-    assert 'spectral_axis=<SpectralAxis [' in result
+    assert 'spectral_axis=<SpectralAxis' in result
     assert 'uncertainty=StdDevUncertainty(' in result
 
 