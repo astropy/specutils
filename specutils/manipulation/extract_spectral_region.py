0c4b7c47277476ad8c6075000c6661bf393a9bac
Ivo <busko@stsci.edu>
2021-01-05 11:26:02 -0500

    Add spectral_slab function.

--- specutils/manipulation/extract_spectral_region.py
+++ specutils/manipulation/extract_spectral_region.py
@@ -5,7 +5,7 @@ import numpy as np
 from astropy import units as u
 from .. import Spectrum1D
 
-__all__ = ['extract_region']
+__all__ = ['extract_region', 'spectral_slab']
 
 
 def _to_edge_pixel(subregion, spectrum):
@@ -161,3 +161,33 @@ def extract_region(spectrum, region):
         extracted_spectrum = extracted_spectrum[0]
 
     return extracted_spectrum
+
+
+def spectral_slab(spectrum, region):
+    """
+    Extract a slab from the input `~specutils.Spectrum1D`
+    defined by the lower and upper bounds defined by the ``region``
+    instance.  The extracted region will be returned as a new
+    `~specutils.Spectrum1D`.
+
+    Parameters
+    ----------
+    spectrum: `~specutils.Spectrum1D`
+        The spectrum object from which the region will be extracted.
+
+    region: `~specutils.SpectralRegion`
+        The spectral region to extract from the original spectrum.
+
+    Returns
+    -------
+    spectrum: `~specutils.Spectrum1D` or list of `~specutils.Spectrum1D`
+        Excised spectrum, or list of spectra if the input region contained multiple
+        subregions.
+
+    Notes
+    -----
+    This is for now just a proxy for function `extract_region`, to ease the
+    transition from `spectral-cube`.
+
+    """
+    return extract_region(spectrum, region)
