5fa133a083167bb6de2818e088628b9c4455b20c
Ivo <busko@stsci.edu>
2021-02-11 11:25:26 -0500

    Spline replacement of spectrum segment

--- specutils/manipulation/replace_segment.py
+++ specutils/manipulation/replace_segment.py
@@ -1,25 +1,75 @@
+import numpy as np
+
 from scipy.interpolate import CubicSpline
 
 from ..spectra import Spectrum1D
 
 
-def spline_replace(spectrum, spline_points, extrapolation_treatment='nan_fill'):
+def spline_replace(spectrum, spline_knots, extrapolation_treatment='zero_fill'):
+    """
+    Generates a new spectrum with a region replaced by a smooth spline.
 
-    if extrapolation_treatment not in ('nan_fill', 'zero_fill'):
+    Parameters
+    ----------
+
+    spectrum : `~specutils.Spectrum1D`
+        The spectrum to which we want to set the uncertainty.
+
+    spline_knots : array of `~astropy.units.Quantity`
+        List of spectral axis values to be used as spline knots.
+        They all should share the same units, which can be different
+        from the units of the input spectrum spectral axis.
+
+    extrapolation_treatment : str
+        What to do with data off the edges of the region encompassed by
+        the spline points. Can be ``'data_fill'`` to have points filled
+        with the input flux values, or ``'zero_fill'`` to be set to zero.
+
+    Returns
+    -------
+    spectrum : `~specutils.Spectrum1D`
+        The spectrum with the region replaced by spline values, and data
+        values or zeros outside the spline region. The spectral axis will
+        have the same units as the spline knots.
+
+    Notes
+    -----
+    The uncertainty attribute, if present in the input spectrum, is not
+    carried over into the output.
+
+    Examples
+    --------
+
+    >>> wave_val = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+    >>> flux_val = np.array([2, 4, 6, 8, 10, 12, 14, 16, 18, 20])
+    >>> input_spectrum = Spectrum1D(spectral_axis=wave_val * u.AA, flux=flux_val * u.mJy)
+    >>> spline_knots = [3.5, 4.7, 6.8, 7.1] * u.AA
+    >>> result = spline_replace(input_spectrum, spline_knots,
+    ...     extrapolation_treatment='data_fill') # doctest: +IGNORE_OUTPUT
+
+    """
+    if extrapolation_treatment not in ('data_fill', 'zero_fill'):
         raise ValueError('invalid extrapolation_treatment value: ' + str(extrapolation_treatment))
 
-    # Output spectral axis will have the same units as spline points
-    new_spectral_axis = spectrum.spectral_axis.to(spline_points.unit)
+    # Output spectral axis will have the same units as spline knots
+    new_spectral_axis = spectrum.spectral_axis.to(spline_knots.unit)
 
-    # Interpolate on input data to get flux at each spline point
+    # Interpolate on input data to get flux at each spline knot
     flux_spline = CubicSpline(new_spectral_axis.value, spectrum.flux.value,
-                              extrapolate=extrapolation_treatment != 'nan_fill')
-    spline_points_flux_val = flux_spline(spline_points.value)
+                              extrapolate=False)
+    spline_knots_flux_val = flux_spline(spline_knots.value)
 
-    # Now, evaluate spline over spline points only, and use it
+    # Now, evaluate spline over spline knots only, and use it
     # to compute flux at each spectral value in input spectrum.
-    flux_spline_2 = CubicSpline(spline_points.value, spline_points_flux_val,
-                                extrapolate=extrapolation_treatment != 'nan_fill')
+    flux_spline_2 = CubicSpline(spline_knots.value, spline_knots_flux_val,
+                                extrapolate=False)
     out_flux_val = flux_spline_2(new_spectral_axis.value) * spectrum.flux.unit
 
-    return Spectrum1D(spectral_axis=new_spectral_axis, flux=out_flux_val)
+    # default behavior (zero_fill)
+    out = np.where(np.isnan(out_flux_val), 0., out_flux_val)
+
+    if extrapolation_treatment == 'data_fill':
+        data = np.where(np.isnan(out_flux_val), spectrum.flux.value, 0.)
+        out += data * spectrum.flux.unit
+
+    return Spectrum1D(spectral_axis=new_spectral_axis, flux=out)
--- specutils/tests/test_replace_segment.py
+++ specutils/tests/test_replace_segment.py
@@ -1,25 +1,30 @@
 import numpy as np
 
 import astropy.units as u
-from astropy.nddata import StdDevUncertainty
 from astropy.tests.helper import assert_quantity_allclose
 
 from ..spectra.spectrum1d import Spectrum1D
-
 from ..manipulation.replace_segment import spline_replace
 
 
 def test_replace_spline():
     wave_val = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
     flux_val = np.array([2, 4, 6, 8, 10, 12, 14, 16, 18, 20])
-    uncert_val = np.array([0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0])
 
-    input_spectrum = Spectrum1D(spectral_axis=wave_val * u.AA, flux=flux_val * u.mJy,
-                                uncertainty=StdDevUncertainty(uncert_val*u.mJy))
+    input_spectrum = Spectrum1D(spectral_axis=wave_val * u.AA, flux=flux_val * u.mJy)
 
-    spline_points = [3.5, 4.7, 6.8, 7.1] * u.AA
+    spline_knots = [3.5, 4.7, 6.8, 7.1] * u.AA
 
-    result = spline_replace(input_spectrum, spline_points, extrapolation_treatment='zero_fill')
+    # with data fill, recovers the input flux
+    result = spline_replace(input_spectrum, spline_knots, extrapolation_treatment='data_fill')
 
     assert_quantity_allclose(result.flux, flux_val*u.mJy)
     assert_quantity_allclose(result.spectral_axis, input_spectrum.spectral_axis)
+
+    # with default extrapolation, fills with zeros.
+    result = spline_replace(input_spectrum, spline_knots)
+
+    assert_quantity_allclose(result.flux[0], 0.*u.mJy)
+    assert_quantity_allclose(result.flux[1], 0.*u.mJy)
+    assert_quantity_allclose(result.flux[-1], 0.*u.mJy)
+    assert_quantity_allclose(result.flux[-2], 0.*u.mJy)
