453ddbe7421ba50c12046aecad75eb249401c84f
Ivo <busko@stsci.edu>
2021-02-25 15:24:33 -0500

    Fix doc

--- specutils/manipulation/replace_model.py
+++ specutils/manipulation/replace_model.py
@@ -43,12 +43,12 @@ def model_replace(spectrum, spline_knots, extrapolation_treatment='data_fill',
     >>> import numpy as np
     >>> import astropy.units as u
     >>> from specutils.spectra.spectrum1d import Spectrum1D
-    >>> from specutils.manipulation.replace_segment import spline_replace
+    >>> from specutils.manipulation.replace_model import model_replace
     >>> wave_val = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
     >>> flux_val = np.array([2, 4, 6, 8, 10, 12, 14, 16, 18, 20])
     >>> input_spectrum = Spectrum1D(spectral_axis=wave_val * u.AA, flux=flux_val * u.mJy)
     >>> spline_knots = [3.5, 4.7, 6.8, 7.1] * u.AA
-    >>> result = spline_replace(input_spectrum, spline_knots) # doctest: +IGNORE_OUTPUT
+    >>> result = model_replace(input_spectrum, spline_knots) # doctest: +IGNORE_OUTPUT
 
     """
     if extrapolation_treatment not in ('data_fill', 'zero_fill'):
