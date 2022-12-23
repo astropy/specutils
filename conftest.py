0c1dc8baa0447715793a3c334fd5ab4345b13a9b
Pey Lian Lim (Github) <2090236+pllim@users.noreply.github.com>
2022-12-23 11:47:39 -0500

    TST: tox config revamp to modernize it and remove redundancy.
    Doc build is now completely done by RTD.
    
    Clean up conftest and top init.
    
    Fix pytest header in CI.
    
    doctest to ignore __init__.py
    because it tries to collect in Python 3.11 and crashes.
    
    Make sure oldestdeps not lying.

--- conftest.py
+++ conftest.py
@@ -1,9 +1,34 @@
-from importlib.util import find_spec
 import pkg_resources
+
+try:
+    from pytest_astropy_header.display import PYTEST_HEADER_MODULES, TESTED_VERSIONS
+    ASTROPY_HEADER = True
+except ImportError:
+    ASTROPY_HEADER = False
 
 entry_points = []
 for entry_point in pkg_resources.iter_entry_points('pytest11'):
     entry_points.append(entry_point.name)
 
-if "asdf_schema_tester" not in entry_points and find_spec('asdf') is not None:
+if "asdf_schema_tester" not in entry_points:
     pytest_plugins = ['asdf.tests.schema_tester']
+
+
+# Repeat this from specutils/conftest.py so tox picks it up.
+def pytest_configure(config):
+
+    if ASTROPY_HEADER:
+        config.option.astropy_header = True
+
+        # Customize the following lines to add/remove entries from the list of
+        # packages for which version numbers are displayed when running the tests.
+        PYTEST_HEADER_MODULES.pop('Pandas', None)
+        PYTEST_HEADER_MODULES.pop('h5py', None)
+        PYTEST_HEADER_MODULES['astropy'] = 'astropy'
+        PYTEST_HEADER_MODULES['gwcs'] = 'gwcs'
+        PYTEST_HEADER_MODULES['asdf'] = 'asdf'
+        PYTEST_HEADER_MODULES['ndcube'] = 'ndcube'
+        PYTEST_HEADER_MODULES['spectral-cube'] = 'spectral_cube'
+
+        from specutils import __version__
+        TESTED_VERSIONS['specutils'] = __version__
--- specutils/__init__.py
+++ specutils/__init__.py
@@ -2,32 +2,34 @@
 """
 Specutils: an astropy package for spectroscopy.
 """
+import os
 
-# Packages may add whatever they like to this file, but
-# should keep this content at the top.
-# ----------------------------------------------------------------------------
-from ._astropy_init import *   # noqa
 from astropy import config as _config
-# ----------------------------------------------------------------------------
-
-if not _ASTROPY_SETUP_:  # noqa
-    # For egg_info test builds to pass, put package imports here.
-
-    # Allow loading spectrum object from top level module
-    from .spectra import *  # noqa
-
-    # Load the IO functions
-    from .io.default_loaders import *  # noqa
-    from .io.registers import _load_user_io
-    _load_user_io()
+from astropy.tests.runner import TestRunner
 
 __citation__ = 'https://doi.org/10.5281/zenodo.1421356'
 
+try:
+    from .version import version as __version__
+except ImportError:
+    __version__ = ''
+
+# Create the test function for self test
+test = TestRunner.make_test_runner_in(os.path.dirname(__file__))
+test.__test__ = False
+
+
+# Allow loading spectrum object from top level module
+from .spectra import *  # noqa
+
+# Load the IO functions
+from .io.default_loaders import *  # noqa
+from .io.registers import _load_user_io  # noqa
+_load_user_io()
+
 
 class Conf(_config.ConfigNamespace):
-    """
-    Configuration parameters for specutils.
-    """
+    """Configuration parameters for specutils."""
 
     do_continuum_function_check = _config.ConfigItem(
         True,
@@ -37,3 +39,9 @@ class Conf(_config.ConfigNamespace):
 
 
 conf = Conf()
+
+# Clean up namespace
+del os
+del _config
+del TestRunner
+del _load_user_io
