de40ab59702319e3c7dcc14d0bc567dadbbc62ea
Ricky O'Steen <rosteen@stsci.edu>
2023-03-14 16:50:04 -0400

    Add writing SDSS file back out to test

--- specutils/tests/test_loaders.py
+++ specutils/tests/test_loaders.py
@@ -170,6 +170,9 @@ def test_sdss_spec(tmp_path):
         assert isinstance(spec, Spectrum1D)
         assert spec.flux.size > 0
 
+        # Make sure we can write the spectrum back out
+        spec.write(tmp_path / '_tst.fits')
+
     file_path = str(tmp_path / sp_pattern)
     with urllib.request.urlopen(EBOSS_SPECTRUM_URL) as response:
         with open(file_path, 'wb') as tmp_file:
