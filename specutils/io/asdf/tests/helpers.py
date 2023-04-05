4c9672f22aa67120e519fb8f200a90848f34ad63
P. L. Lim <2090236+pllim@users.noreply.github.com>
2023-04-05 16:19:11 -0400

    ASDF mask serialization (#1053)

--- specutils/io/asdf/tests/helpers.py
+++ specutils/io/asdf/tests/helpers.py
@@ -3,7 +3,7 @@ These are similar to those in ``asdf_astropy.testing.helpers``.
 """
 from asdf_astropy.testing.helpers import assert_spectral_coord_equal
 from astropy.tests.helper import assert_quantity_allclose
-from numpy.testing import assert_allclose
+from numpy.testing import assert_allclose, assert_array_equal
 
 __all__ = ["assert_spectral_axis_equal", "assert_spectrum1d_equal", "assert_spectrumlist_equal"]
 
@@ -21,11 +21,17 @@ def assert_spectrum1d_equal(a, b):
 
     assert_quantity_allclose(a.flux, b.flux)
     assert_spectral_axis_equal(a.spectral_axis, b.spectral_axis)
+
     if a.uncertainty is None:
         assert b.uncertainty is None
     else:
         assert a.uncertainty.uncertainty_type == b.uncertainty.uncertainty_type
         assert_allclose(a.uncertainty.array, b.uncertainty.array)
+
+    if a.mask is None:
+        assert b.mask is None
+    else:
+        assert_array_equal(a.mask, b.mask)
 
 
 def assert_spectrumlist_equal(a, b):
