b6b0f44a80f8a098116eea6be9491600608de2f4
P. L. Lim <2090236+pllim@users.noreply.github.com>
2023-09-12 14:49:46 -0400

    TST: Remove asdf schema_tester entry point

--- conftest.py
+++ conftest.py
@@ -1,17 +1,8 @@
-import pkg_resources
-
 try:
     from pytest_astropy_header.display import PYTEST_HEADER_MODULES, TESTED_VERSIONS
     ASTROPY_HEADER = True
 except ImportError:
     ASTROPY_HEADER = False
-
-entry_points = []
-for entry_point in pkg_resources.iter_entry_points('pytest11'):
-    entry_points.append(entry_point.name)
-
-if "asdf_schema_tester" not in entry_points:
-    pytest_plugins = ['asdf.tests.schema_tester']
 
 
 # Repeat this from specutils/conftest.py so tox picks it up.
