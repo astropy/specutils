91081ad242b9ecd1d4c180bdd474a3ba42f1b436
kelle <kellecruz@gmail.com>
2023-11-22 15:39:07 -0500

    restore old legacy tests.

--- specutils/tests/test_loaders.py
+++ specutils/tests/test_loaders.py
@@ -671,16 +671,25 @@ def test_tabular_fits_maskheader(tmp_path):
     print(f"spetrum meta: \n {repr(spectrum.meta['header'])}")
     # Read it in and check against the original
     with fits.open(tmpfile) as hdulist:
-        print(f'\nhdu info:\n')
-        hdulist.info()
-        print(f'\nhdu0 header:\n{repr(hdulist[0].header)}')
-        print(f'\nhdu1 header:\n{repr(hdulist[1].header)}')
+        # print(f'\nhdu info:\n')
+        # hdulist.info()
+        # print(f'\nhdu0 header:\n{repr(hdulist[0].header)}')
+        # print(f'\nhdu1 header:\n{repr(hdulist[1].header)}')
 
+        # Test HDU0 header
         assert hdulist[0].header['OBSERVER'] == 'Parsons'
+<<<<<<< HEAD
 
         # keys relevant to datashape are in HDU 1
+=======
+    
+        # keys relevant to datashape are in HDU1 header
+>>>>>>> 34257f44 (restore old legacy tests.)
         assert hdulist[1].header['NAXIS'] == 2
         assert hdulist[1].header['NAXIS2'] == disp.shape[0]
+
+        # Test legacy behaviour of metadata is HDU1 header
+        assert hdulist[1].header['OBSERVER'] == 'Parsons'
 
 
 def test_tabular_fits_update_header(tmp_path):
@@ -699,7 +708,7 @@ def test_tabular_fits_update_header(tmp_path):
 
     with fits.open(tmpfile) as hdulist:
         assert hdulist[0].header['OBSERVER'] == 'Rosse'
-        #  assert hdulist[1].header['NAXIS2'] == disp.shape[0]
+        assert hdulist[1].header['NAXIS2'] == disp.shape[0]
         assert_allclose(hdulist[1].header['EXPTIME'], 3.21e1)
 
     # Test that unsupported types (dict) are not added to written header
