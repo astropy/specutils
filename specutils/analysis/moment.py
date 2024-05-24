Correcting moment 0 calculation. Still have one failing test

specutils/analysis/moment.py
@@ -5,6 +5,7 @@ spectral features.
 
 import numpy as np
 from ..manipulation import extract_region
+from ..spectra import SpectralAxis
 from .utils import computation_wrapper
 
 
@@ -61,7 +62,8 @@ def _compute_moment(spectrum, regions=None, order=0, axis=-1):
         return None
 
     if order == 0:
-        return np.sum(flux, axis=axis)
+        dx = np.abs(np.diff(spectral_axis.bin_edges))
+        return np.sum(flux * dx, axis=axis)
 
     dispersion = spectral_axis
     if len(flux.shape) > len(spectral_axis.shape):

specutils/tests/test_analysis.py
@@ -1065,8 +1065,8 @@ def test_moment():
     spectrum = Spectrum1D(spectral_axis=frequencies, flux=flux)
 
     moment_0 = moment(spectrum, order=0)
-    assert moment_0.unit.is_equivalent(u.Jy)
-    assert quantity_allclose(moment_0, 252.96*u.Jy, atol=0.01*u.Jy)
+    assert moment_0.unit.is_equivalent(u.Jy * u.GHz)
+    assert quantity_allclose(moment_0, 2.5045*u.Jy*u.GHz, atol=0.001*u.Jy*u.GHz)
 
     moment_1 = moment(spectrum, order=1)
     assert moment_1.unit.is_equivalent(u.GHz)
@@ -1096,6 +1096,11 @@ def test_moment_cube():
     flux_multid = np.broadcast_to(flux, [9, 10, flux.shape[0]]) * u.Jy
 
     spectrum = Spectrum1D(spectral_axis=frequencies, flux=flux_multid)
+
+    moment_0 = moment(spectrum, order=0)
+
+    assert moment_0.shape == (9, 10)
+    assert moment_0.unit.is_equivalent(u.Jy*u.GHz)
 
     moment_1 = moment(spectrum, order=1)
 